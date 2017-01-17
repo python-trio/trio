f0f0e38e7d1a8869b5704b9285f1f6f66245f43c
Nathaniel J. Smith <njs@pobox.com>
2017-01-16 18:03:25 -0800

    Wrapper for PyErr_SetExcInfo
    probably not useful actually, but I wrote it so I'll stash it here :-)

--- trio/_core/_util.py
+++ trio/_core/_util.py
@@ -1,7 +1,8 @@
 import sys
 from functools import wraps
+import ctypes
 
-__all__ = ["aiter_compat"]
+__all__ = ["aiter_compat", "PyErr_SetExcInfo"]
 
 def aiter_compat(aiter_impl):
     if sys.version_info < (3, 5, 2):
@@ -11,3 +12,23 @@ def aiter_compat(aiter_impl):
         return __aiter__
     else:
         return aiter_impl
+
+
+_ctypes_PyErr_SetExcInfo = ctypes.pythonapi.PyErr_SetExcInfo
+_ctypes_PyErr_SetExcInfo.restype = None
+_ctypes_PyErr_SetExcInfo.argtypes = [ctypes.py_object] * 3
+# PyErr_SetExcInfo steals references to the given objects
+_ctypes_Py_IncRef = ctypes.pythonapi.Py_IncRef
+_ctypes_Py_IncRef.restype = None
+_ctypes_Py_IncRef.argtypes = [ctypes.py_object]
+
+def PyErr_SetExcInfo(exc_info):
+    def prep(arg):
+        if arg is None:
+            # convert None -> NULL
+            return ctypes.py_object()
+        else:
+            _ctypes_Py_IncRef(arg)
+            return arg
+    prepped = [prep(obj) for obj in exc_info]
+    _ctypes_PyErr_SetExcInfo(*prepped)
