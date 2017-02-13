ccf17a7f826f2dda7e45cd75cdd7dc1a86b0e4d2
Nathaniel J. Smith <njs@pobox.com>
2017-02-13 03:23:24 -0800

    Add python_requires + other setup.py tweaks

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ exec(open("trio/_version.py", encoding="utf-8").read())
 
 setup(name="trio",
       version=__version__,
-      description="An async/await-native I/O library",
+      description="An async/await-native I/O library for humans and snake people",
       long_description=open("README.rst", encoding="utf-8").read(),
       author="Nathaniel J. Smith",
       author_email="njs@pobox.com",
       url="https://github.com/njsmith/trio",
-      license="MIT or Apache License, Version 2.0",
+      license="MIT -or- Apache License 2.0",
       packages=find_packages(),
       install_requires=[
           "attrs",
@@ -17,6 +17,7 @@ setup(name="trio",
           "async_generator",
           "cffi; os_name == 'nt'",  # "cffi is required on windows"
       ],
+      python_requires=">=3.5",
       classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
@@ -27,6 +28,7 @@ setup(name="trio",
         "Operating System :: POSIX :: BSD"
         "Operating System :: Microsoft :: Windows"
         "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
