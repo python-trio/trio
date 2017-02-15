858d9c0d00456f1a6d9303ca637eb01e72142454
Nathaniel J. Smith <njs@pobox.com>
2017-02-14 23:44:54 -0800

    Add parking lot repark, __bool__, __len__, comments, tests

--- trio/_core/_parking_lot.py
+++ trio/_core/_parking_lot.py
@@ -1,5 +1,78 @@
-# This doesn't really belong in _core, except it's used by Queue, which is
-# used by KqueueIOManager...
+# ParkingLot provides an abstraction for a fair waitqueue with cancellation
+# and requeueing support. Inspiration:
+#
+#    https://webkit.org/blog/6161/locking-in-webkit/
+#    https://amanieu.github.io/parking_lot/
+#
+# which were in turn heavily influenced by
+#
+#    http://gee.cs.oswego.edu/dl/papers/aqs.pdf
+#
+# Compared to these, our use of cooperative scheduling allows some
+# simplifications (no need for internal locking). On the other hand, the need
+# to support trio's strong cancellation semantics adds some complications
+# (tasks need to know where they're queued so they can cancel). Also, in the
+# above work, the ParkingLot is a global structure that holds a collection of
+# waitqueues keyed by lock address, and which are opportunistically allocated
+# and destroyed as contention arises; this allows the worst-case memory usage
+# for all waitqueues to be O(#tasks). Here we allocate a separate wait queue
+# for each synchronization object, so we're O(#objects + #tasks). This isn't
+# *so* bad since compared to our synchronization objects are heavier than
+# theirs and our tasks are lighter, so for us #objects is smaller and #tasks
+# is larger.
+#
+# This is in the core because for two reasons. First, it's used by
+# UnboundedQueue, and UnboundedQueue is used for a number of things in the
+# core. And second, it's responsible for providing fairness to all of our
+# high-level synchronization primitives (locks, queues, etc.). For now with
+# our FIFO scheduler this is relatively trivial (it's just a FIFO waitqueue),
+# but in the future we ever start support task priorities or fair scheduling
+#
+#    https://github.com/njsmith/trio/issues/32
+#
+# then all we'll have to do is update this. (Well, full-fledged task
+# priorities might also require priority inheritance, which would require more
+# work.)
+#
+# For discussion of data structures to use here, see:
+#
+#     https://github.com/dabeaz/curio/issues/136
+#
+# (and also the articles above). Currently we use a SortedDict ordered by a
+# global monotonic counter that ensures FIFO ordering. The main advantage of
+# this is that it's easy to implement :-). An intrusive doubly-linked list
+# would also be a natural approach, so long as we only handle FIFO ordering.
+#
+# XX: should we switch to the shared global ParkingLot approach?
+#
+# XX: we should probably add support for "parking tokens" to allow for
+# task-fair RWlock (basically: when parking a task needs to be able to mark
+# itself as a reader or a writer, and then a task-fair wakeup policy is, wake
+# the next task, and if it's a reader than keep waking tasks so long as they
+# are readers). Without this I think you can implement write-biased or
+# read-biased RWlocks (by using two parking lots and drawing from whichever is
+# preferred), but not task-fair -- and task-fair plays much more nicely with
+# WFQ. (Consider what happens in the two-lot implementation if you're
+# write-biased but all the pending writers are blocked at the scheduler level
+# by the WFQ logic...)
+# ...alternatively, "phase-fair" RWlocks are pretty interesting:
+#    http://www.cs.unc.edu/~anderson/papers/ecrts09b.pdf
+# Useful summary:
+# https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/ReadWriteLock.html
+#
+# XX: if we do add WFQ, then we might have to drop the current feature where
+# unpark returns the tasks that were unparked. Rationale: suppose that at the
+# time we call unpark, the next task is deprioritized... and then, before it
+# becomes runnable, a new task parks which *is* runnable. Ideally we should
+# immediately wake the new task, and leave the old task on the queue for
+# later. But this means we can't commit to which task we are unparking when
+# unpark is called.
+#
+# XX: maybe drop the ability to specify what unpark returns? It's unused right
+# now. And it might be useful to have it instead return a ticket that can be
+# used to re-park without giving up the place in line?
+#
+# See: https://github.com/njsmith/trio/issues/53
 
 from itertools import count
 import attr
@@ -23,6 +96,7 @@ class _ParkingLotStatistics:
 @_hazmat
 @attr.s(slots=True, cmp=False, hash=False)
 class ParkingLot:
+    # {idx: [task, idx, lot]}
     _parked = attr.ib(default=attr.Factory(SortedDict))
 
     ALL = _AllType()
@@ -30,19 +104,47 @@ class ParkingLot:
     def statistics(self):
         return _ParkingLotStatistics(tasks_waiting=len(self._parked))
 
+    def __len__(self):
+        return len(self._parked)
+
+    def __bool__(self):
+        return bool(self._parked)
+
+    def _deposit_ticket(self, ticket):
+        # On entry, 'ticket' is a 3-list where the first element is the task
+        # and we overwrite the other two.
+        idx = next(_counter)
+        ticket[1] = idx
+        ticket[2] = self
+        self._parked[idx] = ticket
+
     @_core.enable_ki_protection
     async def park(self):
-        idx = next(_counter)
-        self._parked[idx] = _core.current_task()
+        ticket = [_core.current_task(), None, None]
+        self._deposit_ticket(ticket)
         def abort(_):
-            del self._parked[idx]
+            task, idx, lot = ticket
+            del lot._parked[idx]
             return _core.Abort.SUCCEEDED
         return await _core.yield_indefinitely(abort)
 
-    @_core.enable_ki_protection
-    def unpark(self, *, count=ALL, result=_core.Value(None)):
+    def _pop_several(self, count):
         if count is ParkingLot.ALL:
             count = len(self._parked)
         for _ in range(min(count, len(self._parked))):
-            _, task = self._parked.popitem(last=False)
+            _, ticket = self._parked.popitem(last=False)
+            yield ticket
+
+    @_core.enable_ki_protection
+    def unpark(self, *, count=ALL, result=_core.Value(None)):
+        tasks = [task for (task, _, _) in self._pop_several(count)]
+        for task in tasks:
             _core.reschedule(task, result)
+        return tasks
+
+    @_core.enable_ki_protection
+    def repark(self, new_lot, *, count=ALL):
+        if not isinstance(new_lot, ParkingLot):
+            raise TypeError("new_lot must be a ParkingLot")
+        for ticket in self._pop_several(count):
+            new_lot._deposit_ticket(ticket)
--- trio/_core/tests/test_parking_lot.py
+++ trio/_core/tests/test_parking_lot.py
@@ -14,10 +14,14 @@ async def test_parking_lot_basic():
 
     async with _core.open_nursery() as nursery:
         lot = ParkingLot()
+        assert not lot
+        assert len(lot) == 0
         assert lot.statistics().tasks_waiting == 0
         for i in range(3):
             nursery.spawn(waiter, i, lot)
         await busy_wait_for(lambda: len(record) == 3)
+        assert bool(lot)
+        assert len(lot) == 3
         assert lot.statistics().tasks_waiting == 3
         # default is to wake all
         lot.unpark(result=_core.Value(17))
@@ -51,29 +55,29 @@ async def test_parking_lot_basic():
 
     assert repr(ParkingLot.ALL) == "ParkingLot.ALL"
 
+
+async def cancellable_waiter(name, lot, scopes, record):
+    with _core.open_cancel_scope() as scope:
+        scopes[_core.current_task()] = scope
+        record.append("sleep {}".format(name))
+        try:
+            await lot.park()
+        except _core.Cancelled:
+            record.append("cancelled {}".format(name))
+        else:
+            record.append("wake {}".format(name))
+
 async def test_parking_lot_cancel():
     record = []
-
     scopes = {}
-
-    async def waiter(i, lot):
-        with _core.open_cancel_scope() as scope:
-            scopes[_core.current_task()] = scope
-            record.append("sleep {}".format(i))
-            try:
-                await lot.park()
-            except _core.Cancelled:
-                record.append("cancelled {}".format(i))
-            else:
-                record.append("wake {}".format(i))
 
     async with _core.open_nursery() as nursery:
         lot = ParkingLot()
-        w1 = nursery.spawn(waiter, 1, lot)
+        w1 = nursery.spawn(cancellable_waiter, 1, lot, scopes, record)
         await busy_wait_for(lambda: len(record) == 1)
-        w2 = nursery.spawn(waiter, 2, lot)
+        w2 = nursery.spawn(cancellable_waiter, 2, lot, scopes, record)
         await busy_wait_for(lambda: len(record) == 2)
-        w3 = nursery.spawn(waiter, 3, lot)
+        w3 = nursery.spawn(cancellable_waiter, 3, lot, scopes, record)
         await busy_wait_for(lambda: len(record) == 3)
 
         scopes[w2].cancel()
@@ -88,3 +92,43 @@ async def test_parking_lot_cancel():
         "sleep 1", "sleep 2", "sleep 3",
         "cancelled 2", {"wake 1", "wake 3"},
     ])
+
+async def test_parking_lot_repark():
+    record = []
+    scopes = {}
+    lot1 = ParkingLot()
+    lot2 = ParkingLot()
+
+    with pytest.raises(TypeError):
+        lot1.repark([])
+
+    async with _core.open_nursery() as nursery:
+        w1 = nursery.spawn(cancellable_waiter, 1, lot1, scopes, record)
+        await busy_wait_for(lambda: len(record) == 1)
+        w2 = nursery.spawn(cancellable_waiter, 2, lot1, scopes, record)
+        await busy_wait_for(lambda: len(record) == 2)
+        w3 = nursery.spawn(cancellable_waiter, 3, lot1, scopes, record)
+        await busy_wait_for(lambda: len(record) == 3)
+
+        assert len(lot1) == 3
+        lot1.repark(lot2, count=1)
+        assert len(lot1) == 2
+        assert len(lot2) == 1
+        lot2.unpark()
+        await busy_wait_for(lambda: len(record) == 4)
+        assert record == ["sleep 1", "sleep 2", "sleep 3", "wake 1"]
+
+        lot1.repark(lot2)
+        assert len(lot1) == 0
+        assert len(lot2) == 2
+
+        scopes[w2].cancel()
+        await busy_wait_for(lambda: len(record) == 5)
+        assert len(lot2) == 1
+        assert record == [
+            "sleep 1", "sleep 2", "sleep 3", "wake 1", "cancelled 2"]
+
+        lot2.unpark()
+        await busy_wait_for(lambda: len(record) == 6)
+        assert record == [
+            "sleep 1", "sleep 2", "sleep 3", "wake 1", "cancelled 2", "wake 3"]
