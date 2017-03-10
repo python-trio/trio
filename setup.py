5789c5466d2d70d77027d4da226fdefd8590d32c
Nathaniel J. Smith <njs@pobox.com>
2017-03-10 13:00:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,25 @@ from setuptools import setup, find_packages
 
 exec(open("trio/_version.py", encoding="utf-8").read())
 
+LONG_DESC = """
+Trio is an experimental attempt to produce a production-quality,
+`permissively licensed
+<https://github.com/njsmith/trio/blob/master/LICENSE>`__,
+async/await-native I/O library for Python, with an emphasis on
+**usability** and **correctness** – we want to make it *easy* to
+get things *right*.
+
+Docs: https://trio.readthedocs.io
+
+Issues: https://github.com/njsmith/trio/issues
+
+Repository: https://github.com/njsmith/trio
+"""
+
 setup(name="trio",
       version=__version__,
       description="An async/await-native I/O library for humans and snake people",
-      long_description=open("README.rst", encoding="utf-8").read(),
+      long_description=LONG_DESC,
       author="Nathaniel J. Smith",
       author_email="njs@pobox.com",
       url="https://github.com/njsmith/trio",
@@ -19,7 +34,7 @@ setup(name="trio",
       ],
       python_requires=">=3.5",
       classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "License :: OSI Approved :: Apache Software License",
