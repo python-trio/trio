9fe2a7e60b7f85478260f91af3d9c447b3fbf5ea
Nathaniel J. Smith <njs@pobox.com>
2017-05-22 20:34:35 -0700

    [ci] Attempt to add pypy release build test

--- ci/travis.sh
+++ ci/travis.sh
@@ -3,7 +3,7 @@
 set -ex
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    curl -o macpython.pkg https://www.python.org/ftp/python/${MACPYTHON}/python-${MACPYTHON}-macosx10.6.pkg
+    curl -Lo macpython.pkg https://www.python.org/ftp/python/${MACPYTHON}/python-${MACPYTHON}-macosx10.6.pkg
     sudo installer -pkg macpython.pkg -target /
     ls /Library/Frameworks/Python.framework/Versions/*/bin/
     if expr "${MACPYTHON}" : 2; then
@@ -18,10 +18,22 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
 fi
 
 if [ "$USE_PYPY_NIGHTLY" = "1" ]; then
-    curl -o pypy.tar.bz2 http://buildbot.pypy.org/nightly/py3.5/pypy-c-jit-latest-linux64.tar.bz2
+    curl -Lo pypy.tar.bz2 http://buildbot.pypy.org/nightly/py3.5/pypy-c-jit-latest-linux64.tar.bz2
     tar xaf pypy.tar.bz2
     # something like "pypy-c-jit-89963-748aa3022295-linux64"
     PYPY_DIR=$(echo pypy-c-jit-*)
+    PYTHON_EXE=$PYPY_DIR/bin/pypy3
+    $PYTHON_EXE -m ensurepip
+    $PYTHON_EXE -m pip install virtualenv
+    $PYTHON_EXE -m virtualenv testenv
+    source testenv/bin/activate
+fi
+
+if [ "$USE_PYPY_RELEASE" = "1" ]; then
+    curl -Lo pypy.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-5.7.1-beta-linux_x86_64-portable.tar.bz2
+    tar xaf pypy.tar.bz2
+    # something like "pypy3.5-5.7.1-beta-linux_x86_64-portable"
+    PYPY_DIR=$(echo pypy3.5-*)
     PYTHON_EXE=$PYPY_DIR/bin/pypy3
     $PYTHON_EXE -m ensurepip
     $PYTHON_EXE -m pip install virtualenv
