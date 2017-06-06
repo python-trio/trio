2a26335925f711359ec94d41f83e34bf3993bc70
Zack Buhman <zack@buhman.org>
2017-06-06 14:18:52 -0500

    test_file_io: clean up tests
    This removes type-specialized tests entirely, and moves some setup code to
    fixtures.

--- trio/tests/test_file_io.py
+++ trio/tests/test_file_io.py
@@ -7,14 +7,7 @@ from unittest.mock import patch, sentinel
 
 import trio
 from trio import _core
-
-
-concrete_cls = [
-    io.StringIO,
-    io.BytesIO,
-    io.FileIO,
-    io.IOBase
-]
+from trio._file_io._file_io import _FILE_SYNC_ATTRS, _FILE_ASYNC_METHODS
 
 
 @pytest.fixture
@@ -22,45 +15,74 @@ def path(tmpdir):
     return tmpdir.join('test').__fspath__()
 
 
+@pytest.fixture
+def wrapped():
+    return mock.Mock(spec_set=io.StringIO)
+
+
+@pytest.fixture
+def async_file(wrapped):
+    return trio.wrap_file(wrapped)
+
+
 def test_wrap_invalid():
     with pytest.raises(TypeError):
         trio.wrap_file(str())
 
 
-def test_types_forward():
-    inst = trio.AsyncIO(sentinel)
-
-    for attr_name in inst._available_sync_attrs:
-        assert getattr(inst, attr_name) == getattr(sentinel, attr_name)
+def test_wrapped_property(async_file, wrapped):
+    assert async_file.wrapped == wrapped
 
 
-def test_types_forward_invalid():
-    inst = trio.AsyncIO(None)
+def test_sync_attrs_forwarded(async_file, wrapped):
+    for attr_name in async_file._available_sync_attrs:
+        assert getattr(async_file, attr_name) == getattr(wrapped, attr_name)
+
+
+def test_sync_attrs_invalid_not_forwarded(async_file):
+    async_file._wrapped = sentinel
+
+    assert hasattr(async_file.wrapped, 'invalid_attr')
 
     with pytest.raises(AttributeError):
-        inst.nonexistant_attr
+        getattr(async_file, 'invalid_attr')
 
 
-def test_types_forward_in_dir():
+def test_sync_attrs_in_dir():
     inst = trio.AsyncIO(io.StringIO())
 
     assert all(attr in dir(inst) for attr in inst._available_sync_attrs)
 
 
-@pytest.mark.parametrize("cls", zip(concrete_cls))
-async def test_types_wrap(cls):
-    mock_cls = mock.Mock(spec_set=cls)
-    inst = trio.AsyncIO(mock_cls)
+def test_async_methods_generated_once(async_file):
+    for meth_name in async_file._available_async_methods:
+        assert getattr(async_file, meth_name) == getattr(async_file, meth_name)
 
-    for meth_name in inst._available_async_methods:
-        meth = getattr(inst, meth_name)
-        mock_meth = getattr(mock_cls, meth_name)
 
-        value = await meth(sentinel.argument, kw=sentinel.kw)
+def test_async_methods_signature(async_file):
+    # use read as a representative of all async methods
+    assert async_file.read.__name__ == 'read'
+    assert async_file.read.__qualname__ == 'AsyncIO.read'
 
-        mock_meth.assert_called_once_with(sentinel.argument, kw=sentinel.kw)
-        assert value == mock_meth()
-        mock_cls.reset_mock()
+    assert 'io.StringIO.read' in async_file.read.__doc__
+
+
+async def test_async_methods_wrap(async_file, wrapped):
+    skip = ['detach']
+
+    for meth_name in async_file._available_async_methods:
+        if meth_name in skip:
+            continue
+
+        meth = getattr(async_file, meth_name)
+        wrapped_meth = getattr(wrapped, meth_name)
+
+        value = await meth(sentinel.argument, keyword=sentinel.keyword)
+
+        wrapped_meth.assert_called_once_with(sentinel.argument, keyword=sentinel.keyword)
+        assert value == wrapped_meth()
+
+        wrapped.reset_mock()
 
 
 async def test_open_context_manager(path):
@@ -88,14 +110,16 @@ async def test_open_await_context_manager(path):
     assert f.closed
 
 
-async def test_async_iter():
-    string = 'test\nstring\nend'
+async def test_async_iter(async_file):
+    async_file._wrapped = io.StringIO('test\nfoo\nbar')
+    expected = iter(list(async_file.wrapped))
+    async_file.wrapped.seek(0)
 
-    inst = trio.wrap_file(io.StringIO(string))
-
-    expected = iter(string.splitlines(True))
-    async for actual in inst:
+    async for actual in async_file:
         assert actual == next(expected)
+
+    with pytest.raises(StopIteration):
+        next(expected)
 
 
 async def test_close_cancelled(path):
@@ -112,33 +136,9 @@ async def test_detach_rewraps_asynciobase():
     raw = io.BytesIO()
     buffered = io.BufferedReader(raw)
 
-    inst = trio.AsyncIO(buffered)
+    async_file = trio.wrap_file(buffered)
 
-    detached = await inst.detach()
+    detached = await async_file.detach()
 
     assert isinstance(detached, trio.AsyncIO)
-    assert detached._wrapped == raw
-
-
-async def test_async_method_generated_once():
-    inst = trio.wrap_file(io.StringIO())
-
-    for meth_name in inst._available_async_methods:
-        assert getattr(inst, meth_name) == getattr(inst, meth_name)
-
-
-async def test_wrapped_property():
-    wrapped = io.StringIO()
-    inst = trio.wrap_file(wrapped)
-
-    assert inst.wrapped == wrapped
-
-
-async def test_async_method_signature():
-    inst = trio.wrap_file(io.StringIO())
-
-    # use read as a representative of all async methods
-    assert inst.read.__name__ == 'read'
-    assert inst.read.__qualname__ == 'AsyncIO.read'
-
-    assert 'io.StringIO.read' in inst.read.__doc__
+    assert detached.wrapped == raw
