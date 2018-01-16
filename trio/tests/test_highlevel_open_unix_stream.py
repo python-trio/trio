931b108858234084d6749fa36ecfb2c4304f9bd2
Laura F. D <l@veriny.tf>
2018-01-16 13:00:32 +0000

    Rework some tests.

--- trio/tests/test_highlevel_open_unix_stream.py
+++ trio/tests/test_highlevel_open_unix_stream.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import tempfile
 
@@ -11,21 +12,22 @@ except ImportError:
     pytestmark = pytest.mark.skip("Needs unix socket support")
 
 
-async def test_open_unix_socket():
+async def get_server_socket():
     name = Path() / tempfile.gettempdir() / "test.sock"
     try:
         await name.unlink()
     except OSError:
         pass
 
-    # server socket
     serv_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    serv_sock.bind(name.__fspath__())
+    serv_sock.bind(name.__fspath__())  # bpo-32562
     serv_sock.listen(1)
 
-    # for some reason the server socket can accept the connection AFTER it has
-    # been opened, so use that to test here
-    unix_socket = await open_unix_socket(name.__fspath__())
+    return name, serv_sock
+
+
+async def _do_test_on_sock(serv_sock, unix_socket):
+    # shared code between some tests
     client, _ = serv_sock.accept()
     await unix_socket.send_all(b"test")
     assert client.recv(2048) == b"test"
@@ -33,3 +35,23 @@ async def test_open_unix_socket():
     client.sendall(b"response")
     received = await unix_socket.receive_some(2048)
     assert received == b"response"
+
+
+@pytest.mark.xfail
+async def test_open_bad_socket():
+    # mktemp is marked as insecure, but that's okay, we don't want the file to
+    # exist
+    name = os.path.join(tempfile.gettempdir(), tempfile.mktemp())
+    await open_unix_socket(name)
+
+
+async def test_open_unix_socket():
+    name, serv_sock = await get_server_socket()
+    unix_socket = await open_unix_socket(name.__fspath__())
+    await _do_test_on_sock(serv_sock, unix_socket)
+
+
+async def test_open_unix_socket_with_path():
+    name, serv_sock = await get_server_socket()
+    unix_socket = await open_unix_socket(name)
+    await _do_test_on_sock(serv_sock, unix_socket)
