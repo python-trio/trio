3e097d5dff2f2257a37debf6c92f8bbea2dc9f56
Nathaniel J. Smith <njs@pobox.com>
2018-02-12 23:57:06 -0800

    Add our classifier to setup.py

--- setup.py
+++ setup.py
@@ -109,5 +109,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: System :: Networking",
+        "Framework :: Trio",
     ],
 )
