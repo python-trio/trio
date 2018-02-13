614b5e3e5ed289f5d94bf9cd2781ec49d67aa44c
Nathaniel J. Smith <njs@pobox.com>
2018-02-12 21:39:57 -0800

    Coverage fix

--- trio/_core/tests/test_run.py
+++ trio/_core/tests/test_run.py
@@ -1554,7 +1554,7 @@ def test_nice_error_on_bad_calls_to_run_or_spawn():
             import asyncio
 
             @asyncio.coroutine
-            def generator_based_coro():
+            def generator_based_coro():  # pragma: no cover
                 yield from asyncio.sleep(1)
 
             with pytest.raises(TypeError) as excinfo:
