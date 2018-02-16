8f456e8b2d0cd1a11d9956d61da093427ffe901a
Nathaniel J. Smith <njs@pobox.com>
2018-02-15 18:20:35 -0800

    Fix illumos spelling, again
    I don't know what my fingers are thinking.
    
    (Cf gh-442.)

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ chance to give feedback about any compatibility-breaking changes.
 Vital statistics:
 
 * Supported environments: Linux, MacOS, or Windows running some kind of Python
-  3.5-or-better (either CPython or PyPy3 is fine). \\*BSD and illumus/Solaris
+  3.5-or-better (either CPython or PyPy3 is fine). \\*BSD and illumos/Solaris
   likely work too, but are not tested.
 
 * Install: ``python3 -m pip install -U trio`` (or on Windows, maybe
