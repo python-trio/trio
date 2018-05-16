7c0f9240a9185d961b26d0dc9a72cf89cde85cfa
Davide Rizzo <sorcio@gmail.com>
2018-05-16 16:06:41 +0200

    fix pygments style in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 highlight_language = 'python3'
 
