75e74d7b3be08103a2d6e8277ed37847b13b7ed1
Nathaniel J. Smith <njs@pobox.com>
2018-07-16 17:46:20 -0700

    Mention context managers as a thing that closes resources

--- trio/_core/_exceptions.py
+++ trio/_core/_exceptions.py
@@ -110,10 +110,10 @@ class ClosedResourceError(Exception):
     """Raised when attempting to use a resource after it has been closed.
 
     Note that "closed" here means that *your* code closed the resource,
-    generally by calling a method with a name like ``close`` or ``aclose``. If
-    a problem arises elsewhere – for example, because of a network failure, or
-    because a remote peer closed their end of a connection – then that should
-    be indicated by a different exception class, like :exc:`BrokenStreamError`
-    or an :exc:`OSError` subclass.
+    generally by calling a method with a name like ``close`` or ``aclose``, or
+    by exiting a context manager. If a problem arises elsewhere – for example,
+    because of a network failure, or because a remote peer closed their end of
+    a connection – then that should be indicated by a different exception
+    class, like :exc:`BrokenStreamError` or an :exc:`OSError` subclass.
 
     """
