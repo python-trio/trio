d8cfb8e59d83116005ea6269a588e6e47853b2b4
Johannes Maria Frank <jmfrank63@gmail.com>
2018-08-23 00:12:48 +0100

    merged with 0.6.0 rebase

--- trio/_version.py
+++ trio/_version.py
@@ -1,3 +1,7 @@
 # This file is imported from __init__.py and exec'd from setup.py
 
+<<<<<<< HEAD
 __version__ = "0.6.0+dev"
+=======
+__version__ = "0.5.1+dev"
+>>>>>>> Changed import of reexport to explicit and bumped version
