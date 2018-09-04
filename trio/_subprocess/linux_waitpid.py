e8b88612b9e4a6eb6178489a6f52d7d0385d04c3
Laura F. D <l@veriny.tf>
2018-09-04 04:10:40 +0100

    Make requested changes.

--- trio/_subprocess/linux_waitpid.py
+++ trio/_subprocess/linux_waitpid.py
@@ -2,34 +2,18 @@ import attr
 import functools
 import os
 import outcome
-from typing import Dict, Any
+from typing import Any
 
 from .. import _core
 from .._sync import Event
 from .._threads import run_sync_in_worker_thread
 
-# type: Dict[int, WaitpidResult]
-_pending_waitpids = {}
-
 
 @attr.s
-class WaitpidResult:
+class WaitpidState:
+    pid = attr.ib()
     event = attr.ib(default=attr.Factory(Event))
     outcome = attr.ib(default=None)
-    _cached_result = attr.ib(default=None)
-
-    def unwrap(self):
-        if self._cached_result is not None:
-            if isinstance(self._cached_result, BaseException):
-                raise self._cached_result
-            return self._cached_result
-
-        try:
-            self._cached_result = self.outcome.unwrap()
-            return self._cached_result
-        except BaseException as e:
-            self._cached_result = e
-            raise e from None
 
 
 # https://github.com/python-trio/trio/issues/618
@@ -46,40 +30,26 @@ waitpid_limiter = StubLimiter()
 
 # adapted from
 # https://github.com/python-trio/trio/issues/4#issuecomment-398967572
-async def _task(pid: int) -> None:
+async def _task(state: WaitpidState) -> None:
     """The waitpid thread runner task. This must be spawned as a system
     task."""
     partial = functools.partial(
         os.waitpid,  # function
-        pid,  # pid
+        state.pid,  # pid
         0  # no options
     )
-    try:
-        tresult = await run_sync_in_worker_thread(
-            outcome.capture,
-            partial,
-            cancellable=True,
-            limiter=waitpid_limiter
-        )
-    except Exception as e:
-        result = _pending_waitpids.pop(pid)
-        result.outcome = outcome.Error(e)
-        result.event.set()
-        raise
-    else:
-        result = _pending_waitpids.pop(pid)
-        result.outcome = tresult
-        result.event.set()
+
+    tresult = await run_sync_in_worker_thread(
+        outcome.capture, partial, cancellable=True, limiter=waitpid_limiter
+    )
+    state.outcome = tresult
+    state.event.set()
 
 
 async def waitpid(pid: int) -> Any:
     """Waits for a child process with the specified PID to finish running."""
-    try:
-        waiter = _pending_waitpids[pid]
-    except KeyError:
-        waiter = WaitpidResult()
-        _pending_waitpids[pid] = waiter
-        _core.spawn_system_task(_task, pid)
+    waiter = WaitpidState(pid=pid)
+    _core.spawn_system_task(_task, waiter)
 
     await waiter.event.wait()
-    return waiter.unwrap()
+    return waiter.outcome.unwrap()
--- trio/tests/subprocess/test_waitpid_linux.py
+++ trio/tests/subprocess/test_waitpid_linux.py
@@ -16,33 +16,24 @@ async def test_waitpid():
     pid = os.spawnvp(os.P_NOWAIT, "/bin/false", ("false",))
     result = await waitpid(pid)
     # exit code is a 16-bit int: (code, signal)
-    assert result == (pid, 256)
+    assert result[0] == pid
+    assert os.WIFEXITED(result[1]) and os.WEXITSTATUS(result[1]) == 1
 
     pid2 = os.spawnvp(os.P_NOWAIT, "/bin/true", ("true",))
     result = await waitpid(pid2)
-    assert result == (pid2, 0)
+    assert result[0] == pid2
+    assert os.WIFEXITED(result[1]) and os.WEXITSTATUS(result[1]) == 0
 
     pid3 = os.spawnvp(os.P_NOWAIT, "/bin/sleep", ("/bin/sleep", "5"))
     os.kill(pid3, signal.SIGKILL)
     result = await waitpid(pid3)
-    assert result == (pid3, 9)
-
-
-async def test_waitpid_multiple_accesses():
-    pid = os.spawnvp(os.P_NOWAIT, "/bin/sleep", ("/bin/sleep", "5"))
-
-    async def waiter():
-        result = await waitpid(pid)
-        assert result == (pid, 9)
-
-    async with _core.open_nursery() as n:
-        n.start_soon(waiter)
-        n.start_soon(waiter)
-
-        os.kill(pid, signal.SIGKILL)
+    assert result[0] == pid3
+    status = os.WTERMSIG(result[1])
+    assert os.WIFSIGNALED(result[1]) and status == 9
 
 
 async def test_waitpid_no_process():
     with pytest.raises(ChildProcessError):
-        # this PID probably doesn't exist
-        await waitpid(100000)
+        # this PID does exist, but it's ourselves
+        # which doesn't work
+        await waitpid(os.getpid())
