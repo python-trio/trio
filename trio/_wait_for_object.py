3ebd73d74793e32e87d23190066cb891ee02112e
Brian Merrell <merrellb@Brians-MBP.lan>
2018-10-09 02:21:29 -0400

    Repace StubListener with CapacityLimiter(math.inf)

--- trio/_wait_for_object.py
+++ trio/_wait_for_object.py
@@ -1,17 +1,11 @@
+import math
 from . import _timeouts
 from . import _core
 from ._threads import run_sync_in_worker_thread
 from ._core._windows_cffi import ffi, kernel32, ErrorCodes, raise_winerror, _handle
+from ._sync import CapacityLimiter
 
 __all__ = ["WaitForSingleObject"]
-
-
-class StubLimiter:
-    def release_on_behalf_of(self, x):
-        pass
-
-    async def acquire_on_behalf_of(self, x):
-        pass
 
 
 async def WaitForSingleObject(obj):
@@ -45,7 +39,7 @@ async def WaitForSingleObject(obj):
             handle,
             cancel_handle,
             cancellable=True,
-            limiter=StubLimiter(),
+            limiter=CapacityLimiter(math.inf),
         )
     finally:
         # Clean up our cancel handle. In case we get here because this task was
