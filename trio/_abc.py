e642c7db5ef2c76ab7e5f860ab281234a047bb76
Quentin Pradet <quentin.pradet@gmail.com>
2018-10-24 10:26:14 +0400

    Fix flake8 3.6.0 errors

--- trio/_abc.py
+++ trio/_abc.py
@@ -674,7 +674,7 @@ class ReceiveChannel(AsyncResource):
 
     @abstractmethod
     def clone(self):
-        """Clone this receive channel object.
+        r"""Clone this receive channel object.
 
         This returns a new :class:`ReceiveChannel` object, which acts as a
         duplicate of the original: receiving on the new object does exactly
