21cd62254b6f59131e318031891d7bdff8727b35
Johannes Maria Frank <jmfrank63@gmail.com>
2018-10-28 22:55:10 +0100

    Moved more non windows symbols

--- trio/hazmat.py
+++ trio/hazmat.py
@@ -13,13 +13,12 @@ import sys
 from ._core import (
     cancel_shielded_checkpoint, Abort, wait_task_rescheduled,
     enable_ki_protection, disable_ki_protection, currently_ki_protected, Task,
-    checkpoint, current_task, ParkingLot, UnboundedQueue, RunVar,
-    notify_fd_close, wait_socket_readable, wait_socket_writable,
-    notify_socket_close, TrioToken, current_trio_token,
-    temporarily_detach_coroutine_object, permanently_detach_coroutine_object,
-    reattach_detached_coroutine_object, current_statistics, reschedule,
-    remove_instrument, add_instrument, current_clock, current_root_task,
-    checkpoint_if_cancelled, spawn_system_task
+    checkpoint, current_task, ParkingLot, UnboundedQueue, RunVar, TrioToken,
+    current_trio_token, temporarily_detach_coroutine_object,
+    permanently_detach_coroutine_object, reattach_detached_coroutine_object,
+    current_statistics, reschedule, remove_instrument, add_instrument,
+    current_clock, current_root_task, checkpoint_if_cancelled,
+    spawn_system_task
 )
 
 try:
@@ -36,6 +35,10 @@ try:
         # non windows symbols
         wait_writable,
         wait_readable,
+        notify_fd_close,
+        wait_socket_readable,
+        wait_socket_writable,
+        notify_socket_close
     )
 except ImportError:
     pass
