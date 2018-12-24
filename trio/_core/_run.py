e6058b30ffcf577a86227d9b54a51b8fbec49f0c
Johannes-Maria Frank <jmfrank63@gmail.com>
2018-12-24 14:27:45 +0100

    Fixed docs formatting error

--- trio/_core/_run.py
+++ trio/_core/_run.py
@@ -762,7 +762,7 @@ class Runner:
 
         Args:
           task (trio.hazmat.Task): the task to be rescheduled. Must be blocked
-            in a call to :func:`wait_task_rescheduled`.
+          in a call to :func:`wait_task_rescheduled`.
           next_send (outcome.Outcome): the value (or error) to return (or
             raise) from :func:`wait_task_rescheduled`.
 
