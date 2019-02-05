77edcbbcc15a113ef9ff9a6e16a1381c6a1ef130
Joshua Oreman <oremanj@gmail.com>
2019-02-05 10:18:25 -0800

    add test of instantiating a SendChannel[T] subclass

--- trio/tests/test_abc.py
+++ trio/tests/test_abc.py
@@ -19,3 +19,15 @@ async def test_AsyncResource_defaults():
         assert myar.record == []
 
     assert myar.record == ["ac"]
+
+
+def test_abc_generics():
+    class SomeChannel(tabc.SendChannel[tabc.Stream]):
+        def send_nowait(self, value): raise RuntimeError
+        async def send(self, value): raise RuntimeError
+        def clone(self): raise RuntimeError
+        async def aclose(self): pass
+
+    channel = SomeChannel()
+    with pytest.raises(RuntimeError):
+        channel.send_nowait(None)
