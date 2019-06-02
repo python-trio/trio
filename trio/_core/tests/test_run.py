3e55efed0754ade954f9501140b20c3bc5b22fc3
Nathaniel J. Smith <njs@pobox.com>
2019-06-02 00:14:40 -0700

    maybe this will help?

--- trio/_core/tests/test_run.py
+++ trio/_core/tests/test_run.py
@@ -1630,7 +1630,7 @@ async def test_current_effective_deadline(mock_clock):
 
 
 # @coroutine is deprecated since python 3.8, which is fine with us.
-@pytest.mark.filterwarnings("ignore:@coroutine:DeprecationWarning")
+@pytest.mark.filterwarnings("ignore:.*@coroutine.*:DeprecationWarning")
 def test_nice_error_on_bad_calls_to_run_or_spawn():
     def bad_call_run(*args):
         _core.run(*args)
