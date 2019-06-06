6c232ae005ef5c43b628a9d4a5e9034b18bad1a7
Nathaniel J. Smith <njs@pobox.com>
2019-06-05 21:16:40 -0700

    Disable coverage on pypy3 release

--- ci.sh
+++ ci.sh
@@ -124,9 +124,15 @@ else
 
     # Disable coverage on 3.8 until we run 3.8 on Windows CI too
     #   https://github.com/python-trio/trio/pull/784#issuecomment-446438407
-    if [[ "$(python -V)" != Python\ 3.8* ]]; then
-        # Special flag for pypy3.6 builds, so we can track weird coverage
-        # issues: https://bitbucket.org/pypy/pypy/issues/2943/
+    if [[ "$(python -V)" = Python\ 3.8* ]]; then
+        true;
+    # coverage is broken in pypy3 7.1.1, but is fixed in nightly and should be
+    # fixed in the next release after 7.1.1.
+    # See: https://bitbucket.org/pypy/pypy/issues/2943/
+    elif [[ "$TRAVIS_PYTHON_VERSION" = "pypy3" ]]; then
+        true;
+    else
+        # Flag pypy and cpython coverage differently, until it settles down...
         FLAG="cpython"
         if [[ "$PYPY_NIGHTLY_BRANCH" == "py3.6" ]]; then
             FLAG="pypy36nightly"
