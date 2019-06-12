6449937c125ce061c027889c16c627a0bd35354d
Emile Snyder <emile.snyder@gmail.com>
2019-06-12 08:42:36 -0700

    Drop usage of SO_EXCLUSIVEADDRUSE on win32 (#928)

--- trio/tests/test_highlevel_open_tcp_listeners.py
+++ trio/tests/test_highlevel_open_tcp_listeners.py
@@ -122,7 +122,7 @@ async def test_open_tcp_listeners_rebind():
     sockaddr1 = l1.socket.getsockname()
 
     # Plain old rebinding while it's still there should fail, even if we have
-    # SO_REUSEADDR set (requires SO_EXCLUSIVEADDRUSE on Windows)
+    # SO_REUSEADDR set
     probe = stdlib_socket.socket()
     probe.setsockopt(stdlib_socket.SOL_SOCKET, stdlib_socket.SO_REUSEADDR, 1)
     with pytest.raises(OSError):
