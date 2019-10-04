e324607c4b306e609a6dbb198be55c2a70ed0d18
Nathaniel J. Smith <njs@pobox.com>
2019-10-04 00:48:47 -0700

    More experiments

--- ci.sh
+++ ci.sh
@@ -143,7 +143,8 @@ else
         #   bash <(curl ...)
         # but azure is broken:
         #   https://developercommunity.visualstudio.com/content/problem/743824/bash-task-on-windows-suddenly-fails-with-bash-devf.html
-        curl -v --retry 5 -o codecov.sh https://codecov.io/bash
+        curl --version
+        curl -v --connect-timeout 5 --retry 5 -o codecov.sh https://codecov.io/bash
         bash codecov.sh -n "${CODECOV_NAME}" -F "$FLAG"
     fi
 fi
