e1c0231353e341f7bfe0071c097b65c46afdceb2
Nathaniel J. Smith <njs@pobox.com>
2019-10-20 22:16:59 -0700

    Improve readability
    Rename 'cs' based on review feedback, and expand the comment a bit.

--- trio/_core/_run.py
+++ trio/_core/_run.py
@@ -253,21 +253,23 @@ class CancelStatus:
         )
 
     def recalculate(self):
-        # This is basically a recursive algorithm, but we do it iteratively to
-        # avoid any issues with stack overflow.
+        # This does a depth-first traversal over this and descendent cancel
+        # statuses, to ensure their state is up-to-date. It's basically a
+        # recursive algorithm, but we use an explicit stack to avoid any
+        # issues with stack overflow.
         todo = [self]
         while todo:
-            cs = todo.pop()
+            current = todo.pop()
             new_state = (
-                cs._scope.cancel_called
-                or cs.parent_cancellation_is_visible_to_us
+                current._scope.cancel_called
+                or current.parent_cancellation_is_visible_to_us
             )
-            if new_state != cs.effectively_cancelled:
-                cs.effectively_cancelled = new_state
+            if new_state != current.effectively_cancelled:
+                current.effectively_cancelled = new_state
                 if new_state:
-                    for task in cs._tasks:
+                    for task in current._tasks:
                         task._attempt_delivery_of_any_pending_cancel()
-                todo.extend(cs._children)
+                todo.extend(current._children)
 
     def _mark_abandoned(self):
         self.abandoned_by_misnesting = True
