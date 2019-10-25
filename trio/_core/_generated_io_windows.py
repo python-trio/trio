8c549f32409d1db759009ace9a99c3daae7b40dd
Nathaniel J. Smith <njs@pobox.com>
2019-10-25 02:51:11 -0700

    Re-run gen_exports.py

--- trio/_core/_generated_io_windows.py
+++ trio/_core/_generated_io_windows.py
@@ -6,41 +6,6 @@ from ._ki import LOCALS_KEY_KI_PROTECTION_ENABLED
 
     
 
-def current_iocp():
-    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
-    try:
-        return  GLOBAL_RUN_CONTEXT.runner.io_manager.current_iocp()
-    except AttributeError:
-        raise RuntimeError('must be called from async context')
-
-def register_with_iocp(handle):
-    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
-    try:
-        return  GLOBAL_RUN_CONTEXT.runner.io_manager.register_with_iocp(handle)
-    except AttributeError:
-        raise RuntimeError('must be called from async context')
-
-def notify_closing(handle):
-    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
-    try:
-        return  GLOBAL_RUN_CONTEXT.runner.io_manager.notify_closing(handle)
-    except AttributeError:
-        raise RuntimeError('must be called from async context')
-
-async def wait_overlapped(handle, lpOverlapped):
-    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
-    try:
-        return await GLOBAL_RUN_CONTEXT.runner.io_manager.wait_overlapped(handle, lpOverlapped)
-    except AttributeError:
-        raise RuntimeError('must be called from async context')
-
-def monitor_completion_key():
-    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
-    try:
-        return  GLOBAL_RUN_CONTEXT.runner.io_manager.monitor_completion_key()
-    except AttributeError:
-        raise RuntimeError('must be called from async context')
-
 async def wait_readable(sock):
     locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
     try:
@@ -55,6 +20,27 @@ async def wait_writable(sock):
     except AttributeError:
         raise RuntimeError('must be called from async context')
 
+def notify_closing(handle):
+    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
+    try:
+        return  GLOBAL_RUN_CONTEXT.runner.io_manager.notify_closing(handle)
+    except AttributeError:
+        raise RuntimeError('must be called from async context')
+
+def register_with_iocp(handle):
+    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
+    try:
+        return  GLOBAL_RUN_CONTEXT.runner.io_manager.register_with_iocp(handle)
+    except AttributeError:
+        raise RuntimeError('must be called from async context')
+
+async def wait_overlapped(handle, lpOverlapped):
+    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
+    try:
+        return await GLOBAL_RUN_CONTEXT.runner.io_manager.wait_overlapped(handle, lpOverlapped)
+    except AttributeError:
+        raise RuntimeError('must be called from async context')
+
 async def write_overlapped(handle, data, file_offset=0):
     locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
     try:
@@ -66,5 +52,19 @@ async def readinto_overlapped(handle, buffer, file_offset=0):
     locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
     try:
         return await GLOBAL_RUN_CONTEXT.runner.io_manager.readinto_overlapped(handle, buffer, file_offset)
+    except AttributeError:
+        raise RuntimeError('must be called from async context')
+
+def current_iocp():
+    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
+    try:
+        return  GLOBAL_RUN_CONTEXT.runner.io_manager.current_iocp()
+    except AttributeError:
+        raise RuntimeError('must be called from async context')
+
+def monitor_completion_key():
+    locals()[LOCALS_KEY_KI_PROTECTION_ENABLED] = True
+    try:
+        return  GLOBAL_RUN_CONTEXT.runner.io_manager.monitor_completion_key()
     except AttributeError:
         raise RuntimeError('must be called from async context')
