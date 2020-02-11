e8659acad6251b6a79e5db1fb5194796948fcb15
lewoudar <lewoudar@gmail.com>
2020-02-11 21:25:34 +0100

    Change implementation to check filename type
    Remove TypeError in docstring

--- trio/_highlevel_open_unix_stream.py
+++ trio/_highlevel_open_unix_stream.py
@@ -36,18 +36,14 @@ async def open_unix_socket(filename,):
     Raises:
       OSError: If the socket file could not be connected to.
       RuntimeError: If AF_UNIX sockets are not supported.
-      TypeError: if filename is not str or bytes.
     """
     if not has_unix:
         raise RuntimeError("Unix sockets are not supported on this platform")
-
-    if not isinstance(filename, (str, trio.Path, bytes)):
-        raise TypeError("Filename must be str, trio.Path or bytes")
 
     # much more simplified logic vs tcp sockets - one socket type and only one
     # possible location to connect to
     sock = socket(AF_UNIX, SOCK_STREAM)
     with close_on_error(sock):
-        await sock.connect(filename)
+        await sock.connect(trio._util.fspath(filename))
 
     return trio.SocketStream(sock)
