316b640a954bab3a9cbe7afcb20ee39706db528c
Quentin Pradet <quentin.pradet@gmail.com>
2020-06-25 09:35:20 +0400

    Just use typing.Dict instead of DefaultDict

--- trio/_core/_io_epoll.py
+++ trio/_core/_io_epoll.py
@@ -1,7 +1,7 @@
 import select
 import attr
 from collections import defaultdict
-from typing import DefaultDict
+from typing import Dict
 
 from .. import _core
 from ._run import _public
@@ -186,7 +186,7 @@ class EpollIOManager:
     _epoll = attr.ib(factory=select.epoll)
     # {fd: EpollWaiters}
     _registered = attr.ib(
-        factory=lambda: defaultdict(EpollWaiters), type=DefaultDict[int, EpollWaiters]
+        factory=lambda: defaultdict(EpollWaiters), type=Dict[int, EpollWaiters]
     )
     _force_wakeup = attr.ib(factory=WakeupSocketpair, type=WakeupSocketpair)
     _force_wakeup_fd = attr.ib(default=None)
