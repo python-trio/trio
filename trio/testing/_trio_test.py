db91eca22ef696998110d2bbf2ab7becf64204d1
Kyle Altendorf <sda@fstab.net>
2020-08-26 21:41:17 -0400

    Preliminary configurable trio.run for trio_test()

--- trio/testing/_trio_test.py
+++ trio/testing/_trio_test.py
@@ -12,18 +12,24 @@ from ..abc import Clock, Instrument
 #
 # Also: if a pytest fixture is passed in that subclasses the Clock abc, then
 # that clock is passed to trio.run().
-def trio_test(fn):
-    @wraps(fn)
-    def wrapper(**kwargs):
-        __tracebackhide__ = True
-        clocks = [c for c in kwargs.values() if isinstance(c, Clock)]
-        if not clocks:
-            clock = None
-        elif len(clocks) == 1:
-            clock = clocks[0]
-        else:
-            raise ValueError("too many clocks spoil the broth!")
-        instruments = [i for i in kwargs.values() if isinstance(i, Instrument)]
-        return _core.run(partial(fn, **kwargs), clock=clock, instruments=instruments)
+def trio_test(fn=None, *, run=_core.run):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(**kwargs):
+            __tracebackhide__ = True
+            clocks = [c for c in kwargs.values() if isinstance(c, Clock)]
+            if not clocks:
+                clock = None
+            elif len(clocks) == 1:
+                clock = clocks[0]
+            else:
+                raise ValueError("too many clocks spoil the broth!")
+            instruments = [i for i in kwargs.values() if isinstance(i, Instrument)]
+            return run(partial(fn, **kwargs), clock=clock, instruments=instruments)
 
-    return wrapper
+        return wrapper
+
+    if fn is None:
+        return decorator
+
+    return decorator(fn)
