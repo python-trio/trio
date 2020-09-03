d45a972bd97bdfb89270407dffdd3a5189e037c1
Kyle Altendorf <sda@fstab.net>
2020-09-02 22:11:32 -0400

    macOS only supports -i, not --in-place apparently

--- ci.sh
+++ ci.sh
@@ -396,8 +396,8 @@ python -m pip install dist/*.zip
 if python -c 'import sys; sys.exit(sys.version_info >= (3, 7))'; then
     # Python < 3.7, select last ipython with 3.6 support
     # macOS requires the suffix for --in-place or you get an undefined label error
-    sed --in-place='.bak' 's/ipython==[^ ]*/ipython==7.16.1/' test-requirements.txt
-    sed --in-place='.bak' 's/traitlets==[^ ]*/traitlets==4.3.3/' test-requirements.txt
+    sed -i '.bak' 's/ipython==[^ ]*/ipython==7.16.1/' test-requirements.txt
+    sed -i '.bak' 's/traitlets==[^ ]*/traitlets==4.3.3/' test-requirements.txt
     git diff test-requirements.txt
 fi
 
