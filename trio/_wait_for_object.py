947a2f653ed6a2642ac19516b07a18bf74f6b362
richardsheridan <richard.sheridan@gmail.com>
2020-11-18 11:17:19 -0500

    Pool WaitForSingleObject calls
    Up to 64 handles can be waited on per thread, although the threads are handled outside of Trio
    
    Implemented with RegisterWaitForSingleObject and UnregisterWait from kernel32

--- trio/_wait_for_object.py
+++ trio/_wait_for_object.py
@@ -1,62 +1,97 @@
-import math
-from . import _timeouts
-import trio
+from . import _core
 from ._core._windows_cffi import (
     ffi,
     kernel32,
-    ErrorCodes,
+    WaitFlags,
     raise_winerror,
     _handle,
+    _is_signaled,
 )
+
+
+@ffi.callback("WAITORTIMERCALLBACK")
+def wait_callback(context, timer_or_wait_fired):
+    ffi.from_handle(context)()
+
+
+def UnregisterWait(cancel_token):
+    """Python wrapper for kernel32.UnregisterWait.
+
+    Args:
+      cancel_token: Whatever was returned by RegisterWaitForSingleObject.
+
+    """
+    cancel_token, context_handle = cancel_token
+    # have to dereference cancel token i.e. PHANDLE -> HANDLE
+    return kernel32.UnregisterWait(cancel_token[0])
+
+
+def RegisterWaitForSingleObject(handle, callback):
+    """Python wrapper for kernel32.RegisterWaitForSingleObject.
+
+    Args:
+      handle: A valid Win32 handle. This should be guaranteed by WaitForSingleObject.
+
+      callback: A Python function taking no arguments and definitely not raising
+        any errors.
+
+    Returns:
+      cancel_token: An opaque object that can be used with UnregisterWait.
+        This object must be kept alive until the callback is called or cancelled!
+
+    Callbacks are run with WT_EXECUTEINWAITTHREAD | WT_EXECUTEONLYONCE.
+
+    Callbacks are run in a windows system thread, so they must not raise errors.
+
+    """
+    cancel_token = ffi.new("PHANDLE")
+    context_handle = ffi.new_handle(callback)
+    timeout = 0xFFFFFFFF  # INFINITE
+    if not kernel32.RegisterWaitForSingleObject(
+        cancel_token,
+        handle,
+        wait_callback,
+        context_handle,
+        timeout,
+        WaitFlags.WT_EXECUTEINWAITTHREAD | WaitFlags.WT_EXECUTEONLYONCE,
+    ):
+        raise_winerror()
+    # keep context alive by passing it around with cancel_token
+    return cancel_token, context_handle
 
 
 async def WaitForSingleObject(obj):
     """Async and cancellable variant of WaitForSingleObject. Windows only.
 
     Args:
-      handle: A Win32 handle, as a Python integer.
+      obj: A Win32 handle, as a Python integer.
 
     Raises:
       OSError: If the handle is invalid, e.g. when it is already closed.
 
     """
+    await _core.checkpoint_if_cancelled()
     # Allow ints or whatever we can convert to a win handle
     handle = _handle(obj)
 
-    # Quick check; we might not even need to spawn a thread. The zero
-    # means a zero timeout; this call never blocks. We also exit here
+    # Quick check; we might not even need to register the handle.  We also exit here
     # if the handle is already closed for some reason.
-    retcode = kernel32.WaitForSingleObject(handle, 0)
-    if retcode == ErrorCodes.WAIT_FAILED:
-        raise_winerror()
-    elif retcode != ErrorCodes.WAIT_TIMEOUT:
+    if _is_signaled(handle):
+        await _core.cancel_shielded_checkpoint()
         return
 
-    # Wait for a thread that waits for two handles: the handle plus a handle
-    # that we can use to cancel the thread.
-    cancel_handle = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    try:
-        await trio.to_thread.run_sync(
-            WaitForMultipleObjects_sync,
-            handle,
-            cancel_handle,
-            cancellable=True,
-            limiter=trio.CapacityLimiter(math.inf),
-        )
-    finally:
-        # Clean up our cancel handle. In case we get here because this task was
-        # cancelled, we also want to set the cancel_handle to stop the thread.
-        kernel32.SetEvent(cancel_handle)
-        kernel32.CloseHandle(cancel_handle)
+    task = _core.current_task()
+    trio_token = _core.current_trio_token()
 
+    def wakeup():
+        trio_token.run_sync_soon(_core.reschedule, task, idempotent=True)
 
-def WaitForMultipleObjects_sync(*handles):
-    """Wait for any of the given Windows handles to be signaled."""
-    n = len(handles)
-    handle_arr = ffi.new("HANDLE[{}]".format(n))
-    for i in range(n):
-        handle_arr[i] = handles[i]
-    timeout = 0xFFFFFFFF  # INFINITE
-    retcode = kernel32.WaitForMultipleObjects(n, handle_arr, False, timeout)  # blocking
-    if retcode == ErrorCodes.WAIT_FAILED:
-        raise_winerror()
+    cancel_token = RegisterWaitForSingleObject(handle, wakeup)
+
+    def abort(raise_cancel):
+        if UnregisterWait(cancel_token):
+            return _core.Abort.SUCCEEDED
+        else:
+            return _core.Abort.FAILED
+
+    await _core.wait_task_rescheduled(abort)
--- trio/tests/test_wait_for_object.py
+++ trio/tests/test_wait_for_object.py
@@ -7,125 +7,12 @@ on_windows = os.name == "nt"
 pytestmark = pytest.mark.skipif(not on_windows, reason="windows only")
 
 from .._core.tests.tutil import slow
-import trio
 from .. import _core
 from .. import _timeouts
 
 if on_windows:
     from .._core._windows_cffi import ffi, kernel32
-    from .._wait_for_object import (
-        WaitForSingleObject,
-        WaitForMultipleObjects_sync,
-    )
-
-
-async def test_WaitForMultipleObjects_sync():
-    # This does a series of tests where we set/close the handle before
-    # initiating the waiting for it.
-    #
-    # Note that closing the handle (not signaling) will cause the
-    # *initiation* of a wait to return immediately. But closing a handle
-    # that is already being waited on will not stop whatever is waiting
-    # for it.
-
-    # One handle
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    kernel32.SetEvent(handle1)
-    WaitForMultipleObjects_sync(handle1)
-    kernel32.CloseHandle(handle1)
-    print("test_WaitForMultipleObjects_sync one OK")
-
-    # Two handles, signal first
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    kernel32.SetEvent(handle1)
-    WaitForMultipleObjects_sync(handle1, handle2)
-    kernel32.CloseHandle(handle1)
-    kernel32.CloseHandle(handle2)
-    print("test_WaitForMultipleObjects_sync set first OK")
-
-    # Two handles, signal second
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    kernel32.SetEvent(handle2)
-    WaitForMultipleObjects_sync(handle1, handle2)
-    kernel32.CloseHandle(handle1)
-    kernel32.CloseHandle(handle2)
-    print("test_WaitForMultipleObjects_sync set second OK")
-
-    # Two handles, close first
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    kernel32.CloseHandle(handle1)
-    with pytest.raises(OSError):
-        WaitForMultipleObjects_sync(handle1, handle2)
-    kernel32.CloseHandle(handle2)
-    print("test_WaitForMultipleObjects_sync close first OK")
-
-    # Two handles, close second
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    kernel32.CloseHandle(handle2)
-    with pytest.raises(OSError):
-        WaitForMultipleObjects_sync(handle1, handle2)
-    kernel32.CloseHandle(handle1)
-    print("test_WaitForMultipleObjects_sync close second OK")
-
-
-@slow
-async def test_WaitForMultipleObjects_sync_slow():
-    # This does a series of test in which the main thread sync-waits for
-    # handles, while we spawn a thread to set the handles after a short while.
-
-    TIMEOUT = 0.3
-
-    # One handle
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    t0 = _core.current_time()
-    async with _core.open_nursery() as nursery:
-        nursery.start_soon(
-            trio.to_thread.run_sync, WaitForMultipleObjects_sync, handle1
-        )
-        await _timeouts.sleep(TIMEOUT)
-        # If we would comment the line below, the above thread will be stuck,
-        # and Trio won't exit this scope
-        kernel32.SetEvent(handle1)
-    t1 = _core.current_time()
-    assert TIMEOUT <= (t1 - t0) < 2.0 * TIMEOUT
-    kernel32.CloseHandle(handle1)
-    print("test_WaitForMultipleObjects_sync_slow one OK")
-
-    # Two handles, signal first
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    t0 = _core.current_time()
-    async with _core.open_nursery() as nursery:
-        nursery.start_soon(
-            trio.to_thread.run_sync, WaitForMultipleObjects_sync, handle1, handle2
-        )
-        await _timeouts.sleep(TIMEOUT)
-        kernel32.SetEvent(handle1)
-    t1 = _core.current_time()
-    assert TIMEOUT <= (t1 - t0) < 2.0 * TIMEOUT
-    kernel32.CloseHandle(handle1)
-    kernel32.CloseHandle(handle2)
-    print("test_WaitForMultipleObjects_sync_slow thread-set first OK")
-
-    # Two handles, signal second
-    handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
-    t0 = _core.current_time()
-    async with _core.open_nursery() as nursery:
-        nursery.start_soon(
-            trio.to_thread.run_sync, WaitForMultipleObjects_sync, handle1, handle2
-        )
-        await _timeouts.sleep(TIMEOUT)
-        kernel32.SetEvent(handle2)
-    t1 = _core.current_time()
-    assert TIMEOUT <= (t1 - t0) < 2.0 * TIMEOUT
-    kernel32.CloseHandle(handle1)
-    kernel32.CloseHandle(handle2)
-    print("test_WaitForMultipleObjects_sync_slow thread-set second OK")
+    from .._wait_for_object import WaitForSingleObject
 
 
 async def test_WaitForSingleObject():
