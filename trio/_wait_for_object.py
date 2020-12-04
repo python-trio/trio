cb8766cefcfdfd1135a79523d5de2aa21851bf02
richardsheridan <richard.sheridan@gmail.com>
2020-12-04 15:08:36 -0500

    revive simple paired waitforsingleobject

--- trio/_wait_for_object.py
+++ trio/_wait_for_object.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 from sortedcontainers import SortedKeyList
 
-from . import _core
+from . import _core, _threads, _sync
 from ._core._windows_cffi import (
     ffi,
     kernel32,
@@ -261,7 +261,7 @@ UnregisterWait = UnregisterWait_native
 RegisterWaitForSingleObject = RegisterWaitForSingleObject_native
 
 
-async def WaitForSingleObject(obj):
+async def WaitForSingleObject_pool(obj):
     """Async and cancellable variant of WaitForSingleObject. Windows only.
 
     Args:
@@ -313,3 +313,43 @@ async def WaitForSingleObject(obj):
     # Unconditional unregister if not cancelled. Resource cleanup? MSDN says,
     # "Even wait operations that use WT_EXECUTEONLYONCE must be canceled."
     UnregisterWait(cancel_token)
+
+
+async def WaitForSingleObject_pair(obj):
+    """Async and cancellable variant of WaitForSingleObject. Windows only.
+
+    Args:
+      handle: A Win32 handle, as a Python integer.
+
+    Raises:
+      OSError: If the handle is invalid, e.g. when it is already closed.
+
+    """
+    # Allow ints or whatever we can convert to a win handle
+    handle = _handle(obj)
+
+    # Quick check; we might not even need to spawn a thread. The zero
+    # means a zero timeout; this call never blocks. We also exit here
+    # if the handle is already closed for some reason.
+    retcode = kernel32.WaitForSingleObject(handle, 0)
+    if retcode == ErrorCodes.WAIT_FAILED:
+        raise_winerror()
+    elif retcode != ErrorCodes.WAIT_TIMEOUT:
+        return
+
+    # Wait for a thread that waits for two handles: the handle plus a handle
+    # that we can use to cancel the thread.
+    cancel_handle = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
+    try:
+        await _threads.to_thread_run_sync(
+            WaitForMultipleObjects_sync,
+            handle,
+            cancel_handle,
+            cancellable=True,
+            limiter=_sync.CapacityLimiter(1),
+        )
+    finally:
+        # Clean up our cancel handle. In case we get here because this task was
+        # cancelled, we also want to set the cancel_handle to stop the thread.
+        kernel32.SetEvent(cancel_handle)
+        kernel32.CloseHandle(cancel_handle)
