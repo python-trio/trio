a761fcd0cd97638fda56d4c159b50cafb205e470
rednafi <redowan.nafi@gmail.com>
2020-12-31 05:00:00 +0600

    F-stringified refence-core examples

--- docs/source/reference-core/channels-shutdown.py
+++ docs/source/reference-core/channels-shutdown.py
@@ -9,11 +9,11 @@ async def main():
 async def producer(send_channel):
     async with send_channel:
         for i in range(3):
-            await send_channel.send("message {}".format(i))
+            await send_channel.send(f"message {i}")
 
 async def consumer(receive_channel):
     async with receive_channel:
         async for value in receive_channel:
-            print("got value {!r}".format(value))
+            print("got value {value!r}")
 
 trio.run(main)
