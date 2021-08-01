9ee445e54e2eb3a2e8ef7bb302ee9c57961cc4c0
Lura Skye Revuwution <l@veriny.tf>
2021-08-01 02:57:10 +0100

    Add API for changing the context of a running task.

---
+++ trio/_core/_context.py
@@ -0,0 +1,25 @@
+from contextvars import Context
+
+from async_generator import asynccontextmanager
+
+from . import _run
+
+
+@asynccontextmanager
+async def change_context(context: Context):
+    """Asynchronous context manager to change the :class:`contextvars.Context`
+    for the current task.
+
+    """
+    task = _run.current_task()
+    saved_context = task.context
+    task.context = context
+
+    try:
+        await _run.checkpoint()
+        yield
+    finally:
+        task.context = saved_context
+        # I assume this is right?
+        await _run.cancel_shielded_checkpoint()
+
