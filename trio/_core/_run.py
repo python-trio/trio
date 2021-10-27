bb24a6bacbb8f8ec1f72d07b6221880621347981
Sebastián Ramírez <tiangolo@gmail.com>
2021-10-27 21:41:54 +0200

    ✨ Add support for passing contextvars to thread workers

--- trio/_core/_run.py
+++ trio/_core/_run.py
@@ -1416,7 +1416,9 @@ class Runner:
         if "task_scheduled" in self.instruments:
             self.instruments.call("task_scheduled", task)
 
-    def spawn_impl(self, async_fn, args, nursery, name, *, system_task=False):
+    def spawn_impl(
+        self, async_fn, args, nursery, name, *, system_task=False, context=None
+    ):
 
         ######
         # Make sure the nursery is in working order
@@ -1446,10 +1448,11 @@ class Runner:
             except AttributeError:
                 name = repr(name)
 
-        if system_task:
-            context = self.system_context.copy()
-        else:
-            context = copy_context()
+        if context is None:
+            if system_task:
+                context = self.system_context.copy()
+            else:
+                context = copy_context()
 
         if not hasattr(coro, "cr_frame"):
             # This async function is implemented in C or Cython
@@ -1527,7 +1530,7 @@ class Runner:
     ################
 
     @_public
-    def spawn_system_task(self, async_fn, *args, name=None):
+    def spawn_system_task(self, async_fn, *args, name=None, context=None):
         """Spawn a "system" task.
 
         System tasks have a few differences from regular tasks:
@@ -1570,13 +1573,27 @@ class Runner:
               case is if you're wrapping a function before spawning a new
               task, you might pass the original function as the ``name=`` to
               make debugging easier.
+          context: An optional ``contextvars.Context`` object with context variables
+              to use for this task. You would normally get a copy of the current
+              context with ``context = contextvars.copy_context()`` and then you would
+              pass that ``context`` object here.
 
         Returns:
           Task: the newly spawned task
 
         """
+
+        async def async_fn_wrapper():
+            current_async_library_cvar.set("trio")
+            return await async_fn()
+
         return self.spawn_impl(
-            async_fn, args, self.system_nursery, name, system_task=True
+            async_fn_wrapper,
+            args,
+            self.system_nursery,
+            name,
+            system_task=True,
+            context=context,
         )
 
     async def init(self, async_fn, args):
