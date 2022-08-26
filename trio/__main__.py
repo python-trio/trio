7cbd1705e049e90e2470cf8271a78a348017e8f3
Will Badart <Badart_William@bah.com>
2022-08-26 14:41:15 -0700

    Add `__main__` module with `await`-compatible console
    I recently learned that `python -m asyncio` drops you into an
    interpreter session that knows how to deal with `await` expressions. For
    example,
    
    ```console
    $ python -m asyncio
    asyncio REPL 3.10.6 [...] on darwin
    Use "await" directly instead of "asyncio.run()".
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import asyncio
    >>> await asyncio.sleep(1); print("hi")  # prints after one second
    hi
    ```
    
    This quickly became an important part of my async debugging toolbox.
    
    I've also been learning Trio recently, but I've been missing my async
    console.
    
    This PR takes the script from [`asyncio.__main__`][1] and adapts it to
    use a Trio run loop. With this patch applied, users can run `python -m
    trio` to drop into an interactive interpreter session that lets them
    directly `await` Trio-based async expressions.
    
    ```console
    $ python -m trio
    Trio 0.21.0+dev, Python 3.10.6 [...] on darwin
    Use "await" directly instead of "trio.run()".
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import trio
    >>> await trio.sleep(1); print("hi")  # prints after one second
    hi
    ```
    
    [1]: https://github.com/python/cpython/blob/master/Lib/asyncio/__main__.p
    
    I'm very new to Trio, so I'm sure my approach is suboptimal. In
    particular, I'd really appreciate feedback from the team/ community on:
    
    - not using `threading` with a brand new `trio.run` to evaluate
      awaitable expressions. I found that if I simply used
      `self.nursery.start_soon` in the same thread as `runcode`, `await`
      expressions wouldn't get the chance to be evaluated until the session
      was shutting down (might have been blocked by reading keyboard input,
      if I had to guess). Could there be a solution in `trio`'s threading
      tools?
    - what documentation updates should accompany this PR
    - what sort of tests I should add (since this change implements a brand
      new code path that won't be touched by any existing or future usage of
      `trio` itself, I wasn't sure if it would even be appropriate to _try_
      and test it)
    - anything else to align this addition to the Trio way!
    
    Many thanks for your consideration.

---
+++ trio/__main__.py
@@ -0,0 +1,96 @@
+"""Starts an interactive interpreter session that supports top-level
+`await`-ing.
+
+Modeled after the standard library's `asyncio.__main__`. See:
+
+    https://github.com/python/cpython/blob/master/Lib/asyncio/__main__.py
+"""
+
+import ast
+import code
+import concurrent.futures
+import contextlib
+import inspect
+import sys
+import threading
+import types
+
+import trio
+
+
+class TrioInteractiveConsole(code.InteractiveConsole):
+    def __init__(self, locals, nursery):
+        super().__init__(locals)
+        self.compile.compiler.flags |= ast.PyCF_ALLOW_TOP_LEVEL_AWAIT
+        self.nursery = nursery
+
+    def runcode(self, code):
+        func = types.FunctionType(code, self.locals)
+        future = concurrent.futures.Future()
+
+        try:
+            result = func()
+        except SystemExit:
+            raise
+        except BaseException as exc:
+            future.set_exception(exc)
+        else:
+            if not inspect.iscoroutine(result):
+                future.set_result(result)
+            else:
+                await_in_bg = threading.Thread(
+                    target=trio.run, args=(_await, result, future), daemon=True
+                )
+                await_in_bg.start()
+
+        try:
+            return future.result()
+        except SystemExit:
+            raise
+        except KeyboardInterrupt:
+            self.write("\nKeyboardInterrupt\n")
+        except BaseException:
+            self.showtraceback()
+
+
+async def _await(awaitable, future):
+    try:
+        value = await awaitable
+    except SystemExit:
+        raise
+    except BaseException as exc:
+        future.set_exception(exc)
+    else:
+        future.set_result(value)
+
+
+async def main(repl_locals):
+    async with trio.open_nursery() as nursery:
+        console = TrioInteractiveConsole(repl_locals, nursery)
+        banner = (
+            f"Trio {trio.__version__}, "
+            f"Python {sys.version} on {sys.platform}\n"
+            f'Use "await" directly instead of "trio.run()".\n'
+            f'Type "help", "copyright", "credits" or "license" '
+            f"for more information.\n"
+            f'{getattr(sys, "ps1", ">>> ")}import trio'
+        )
+        console.interact(banner=banner, exitmsg="exiting Trio REPL...")
+
+
+if __name__ == "__main__":
+    with contextlib.suppress(ModuleNotFoundError):
+        import readline
+
+    repl_locals = {"trio": trio}
+    for key in {
+        "__name__",
+        "__package__",
+        "__loader__",
+        "__spec__",
+        "__builtins__",
+        "__file__",
+    }:
+        repl_locals[key] = locals()[key]
+
+    trio.run(main, repl_locals)
