c9a634302786896d4ade5d388e0e23bf38f2fc00
KOLANICH <kolan_n@mail.ru>
2022-10-25 12:09:54 +0300

    Changed the build backend to `hatchling`. Ones who want to use `setuptools` can change `build-system` section of `pyproject.toml`, the specific config for it was kept.

--- ci.sh
+++ ci.sh
@@ -66,7 +66,7 @@ fi
 
 python -c "import sys, struct, ssl; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('#' * 70)"
 
-python -m pip install -U pip setuptools setuptools_scm wheel build
+python -m pip install -U pip hatchling build
 python -m pip --version
 
 python -m build -nsx .
