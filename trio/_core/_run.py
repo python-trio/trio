c082f633d3b71eeb692c0297798e12742d34f5d5
Harald Husum <harald.husum@gmail.com>
2023-01-17 12:43:29 +0100

    Defer bound eval

--- trio/_core/_run.py
+++ trio/_core/_run.py
@@ -51,7 +51,7 @@ DEADLINE_HEAP_MIN_PRUNE_THRESHOLD: FinalT = 1000
 
 _NO_SEND: FinalT = object()
 
-FnT = TypeVar("FnT", bound=Callable[..., Any])
+FnT = TypeVar("FnT", bound="Callable[..., Any]")
 
 # Decorator to mark methods public. This does nothing by itself, but
 # trio/_tools/gen_exports.py looks for it.
