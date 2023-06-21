b793eb1693879b11725ac39faa0423034a706e09
A5rocks <git@helvetica.moe>
2023-06-22 00:09:27 +0900

    Set the COVERAGE_PROCESS_START environment variable

--- ci.sh
+++ ci.sh
@@ -103,11 +103,7 @@ else
     INSTALLDIR=$(python -c "import os, trio; print(os.path.dirname(trio.__file__))")
     cp ../pyproject.toml $INSTALLDIR
 
-    # support subprocess spawning with coverage.py
-    PARTOFPYTHONPATH=$(python -c "import sys; print(sys.path[-1])")
-    echo "import coverage; coverage.process_startup()" | tee -a $PARTOFPYTHONPATH/sitecustomize.py
-
-    if coverage run --rcfile=../.coveragerc -m pytest -r a -p trio._tests.pytest_plugin --junitxml=../test-results.xml --run-slow ${INSTALLDIR} --verbose; then
+    if COVERAGE_PROCESS_START=../.coveragerc coverage run --rcfile=../.coveragerc -m pytest -r a -p trio._tests.pytest_plugin --junitxml=../test-results.xml --run-slow ${INSTALLDIR} --verbose; then
         PASSED=true
     else
         PASSED=false
