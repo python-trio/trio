68c90464dc8d6a2e7ada6abbc97298f36ecdfbd0
A5rocks <git@helvetica.moe>
2023-06-21 22:39:50 +0900

    Oops, point towards sitecustomize.py

--- ci.sh
+++ ci.sh
@@ -104,7 +104,8 @@ else
     cp ../pyproject.toml $INSTALLDIR
 
     # support subprocess spawning with coverage.py
-    echo "import coverage; coverage.process_startup()" >> $(python -c "import sys; print(sys.path[0])")
+    PARTOFPYTHONPATH=$(python -c "import sys; print(sys.path[0])")
+    echo "import coverage; coverage.process_startup()" >> $PARTOFPYTHONPATH/sitecustomize.py
 
     if coverage run --rcfile=../.coveragerc -m pytest -r a -p trio._tests.pytest_plugin --junitxml=../test-results.xml --run-slow ${INSTALLDIR} --verbose; then
         PASSED=true
