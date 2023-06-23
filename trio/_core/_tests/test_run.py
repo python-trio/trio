88bf40aac09468856a3e963a883482eeab246ee0
jakkdl <h6+github@pm.me>
2023-06-23 12:39:21 +0200

    mark test_nursery_cancel_doesnt_create_cyclic_garbage as xfail on 3.12, waiting for upstream issue to get resolved.

--- trio/_core/_tests/test_run.py
+++ trio/_core/_tests/test_run.py
@@ -2279,6 +2279,10 @@ async def test_cancel_scope_exit_doesnt_create_cyclic_garbage():
         gc.garbage.clear()
 
 
+@pytest.mark.xfail(
+    sys.version_info >= (3, 12),
+    reason="Waiting on https://github.com/python/cpython/issues/100964",
+)
 @pytest.mark.skipif(
     sys.implementation.name != "cpython", reason="Only makes sense with refcounting GC"
 )
