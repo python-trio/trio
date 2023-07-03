2773e5475866ef107223348df2eed3a6438475eb
jakkdl <h6+github@pm.me>
2023-07-03 13:37:04 +0200

    fix f401 unused import

--- trio/_timeouts.py
+++ trio/_timeouts.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import math
-from collections.abc import Iterator
 from contextlib import AbstractContextManager, contextmanager
 from typing import TYPE_CHECKING
 
