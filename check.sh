889f9b8f64fdb64dbf89da4c693cdca02f2f2759
Spencer Brown <spencerb21@live.com>
2023-08-23 11:00:52 +1000

    Add grouping headers and summary to check.sh

--- check.sh
+++ check.sh
@@ -5,46 +5,76 @@ set -ex
 EXIT_STATUS=0
 
 # Test if the generated code is still up to date
+echo "::group::Generate Exports"
 python ./trio/_tools/gen_exports.py --test \
     || EXIT_STATUS=$?
+echo "::endgroup::"
 
 # Autoformatter *first*, to avoid double-reporting errors
 # (we'd like to run further autoformatters but *after* merging;
 # see https://forum.bors.tech/t/pre-test-and-pre-merge-hooks/322)
 # autoflake --recursive --in-place .
 # pyupgrade --py3-plus $(find . -name "*.py")
+echo "::group::Black"
 if ! black --check setup.py trio; then
+    echo "* Black found issues" >> $GITHUB_STEP_SUMMARY
     EXIT_STATUS=1
     black --diff setup.py trio
 fi
+echo "::endgroup::"
 
+echo "::group::ISort"
 if ! isort --check setup.py trio; then
+    echo "* isort found issues" >> $GITHUB_STEP_SUMMARY
     EXIT_STATUS=1
     isort --diff setup.py trio
 fi
+echo "::endgroup::"
 
 # Run flake8, configured in pyproject.toml
+echo "::group::Flake8"
 flake8 trio/ || EXIT_STATUS=$?
+echo "::endgroup::"
 
 # Run mypy on all supported platforms
-mypy trio --platform linux || EXIT_STATUS=$?
-mypy trio --platform darwin || EXIT_STATUS=$?  # tests FreeBSD too
-mypy trio --platform win32 || EXIT_STATUS=$?
+MYPY = 0
+echo "::group::Mypy Linux"
+mypy trio --platform linux || MYPY=$?
+echo "::endgroup::"
+echo "::group::Mypy Mac"
+mypy trio --platform darwin || MYPY=$?  # tests FreeBSD too
+echo "::endgroup::"
+echo "::group::Mypy Windows"
+mypy trio --platform win32 || MYPY=$?
+echo "::endgroup::"
+
+if [ $MYPY -ne 0 ]; then
+    echo "* Mypy found type errors" >> $GITHUB_STEP_SUMMARY
+    EXIT_STATUS=1
+fi
 
 # Check pip compile is consistent
+echo "::group::Pip Compile - Tests"
 pip-compile test-requirements.in
+echo "::endgroup::"
+echo "::group::Pip Compile - Docs"
 pip-compile docs-requirements.in
+echo "::endgroup::"
 
 if git status --porcelain | grep -q "requirements.txt"; then
+    echo "::group::requirements.txt changed"
+    echo "* requirements.txt changed" >> $GITHUB_STEP_SUMMARY
     git status --porcelain
     git --no-pager diff --color *requirements.txt
     EXIT_STATUS=1
+    echo "::endgroup::"
 fi
 
 codespell || EXIT_STATUS=$?
 
 python trio/_tests/check_type_completeness.py --overwrite-file || EXIT_STATUS=$?
 if git status --porcelain trio/_tests/verify_types*.json | grep -q "M"; then
+    echo "* Type completeness changed, please update!" >> $GITHUB_STEP_SUMMARY
     echo "Type completeness changed, please update!"
     git --no-pager diff --color trio/_tests/verify_types*.json
     EXIT_STATUS=1
