9043b483a80819c40ffa2dbca45258799197d224
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-10-11 20:58:30 -0500

    Test that do-nothing implementations of hooks exist

--- trio/_tests/test_abc.py
+++ trio/_tests/test_abc.py
@@ -4,6 +4,32 @@ import attr
 import pytest
 
 from .. import abc as tabc
+from ..lowlevel import Task
+
+
+def test_instrument_implements_hook_methods() -> None:
+    attrs = {
+        "before_run": (),
+        "after_run": (),
+        "task_spawned": (Task,),
+        "task_scheduled": (Task,),
+        "before_task_step": (Task,),
+        "after_task_step": (Task,),
+        "task_exited": (Task,),
+        "before_io_wait": (3.3,),
+        "after_io_wait": (3.3,),
+    }
+
+    mayonnaise = tabc.Instrument()
+
+    for method_name, args in attrs.items():
+        assert hasattr(mayonnaise, method_name)
+        method = getattr(mayonnaise, method_name)
+        assert callable(method)
+        try:
+            method(*args)
+        except Exception as exc:
+            raise AssertionError("Raised exception") from exc
 
 
 async def test_AsyncResource_defaults() -> None:
