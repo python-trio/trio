9ec853c37cf38131483a2e447ce32e8e8aaf1459
EXPLOSION <git@helvetica.moe>
2023-10-12 02:51:49 -0400

    Use 3.12 in RTD builder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,8 +125,7 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    # make sure that this matches the rtd build version
-    "python": ("https://docs.python.org/3.11", None),
+    "python": ("https://docs.python.org/3", None),
     "outcome": ("https://outcome.readthedocs.io/en/latest/", None),
     "pyopenssl": ("https://www.pyopenssl.org/en/stable/", None),
     "sniffio": ("https://sniffio.readthedocs.io/en/latest/", None),
