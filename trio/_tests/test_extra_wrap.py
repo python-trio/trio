5d5ffa16699bb044f94f7ca89ed6a6d19da6def2
jakkdl <h6+github@pm.me>
2023-10-25 16:26:44 +0200

    refactor tests

--- trio/_tests/test_extra_wrap.py
+++ trio/_tests/test_extra_wrap.py
@@ -1,5 +1,8 @@
 """These tests are for fixing https://github.com/python-trio/trio/issues/2611"""
+from __future__ import annotations
+
 import sys
+from typing import Awaitable, Callable
 
 import pytest
 
@@ -19,87 +22,51 @@ async def raise_after_started(*, task_status: TaskStatus[None]) -> None:
     raise ValueError
 
 
-async def raise_custom_exception_group_before(*, task_status: TaskStatus[None]) -> None:
-    raise ExceptionGroup("my group", [ValueError()])
-
-
-# interestingly enough, this never did a double wrap
-async def test_strict_before_started() -> None:
-    with pytest.raises(BaseExceptionGroup) as exc:
-        async with trio.open_nursery(strict_exception_groups=True) as nursery:
-            await nursery.start(raise_before)
+def _check_exception(exc: pytest.ExceptionInfo) -> None:
+    assert isinstance(exc.value, BaseExceptionGroup)
     assert len(exc.value.exceptions) == 1
     assert isinstance(exc.value.exceptions[0], ValueError)
 
 
-async def test_no_strict_before_started() -> None:
-    with pytest.raises(ValueError):
-        async with trio.open_nursery(strict_exception_groups=False) as nursery:
-            await nursery.start(raise_before)
+async def main(
+    raiser: Callable[[], Awaitable[None]], strict: bool | None = None
+) -> None:
+    async with trio.open_nursery(strict_exception_groups=strict) as nursery:
+        await nursery.start(raiser)
 
 
-async def test_strict_after_started() -> None:
-    with pytest.raises(BaseExceptionGroup) as exc:
-        async with trio.open_nursery(strict_exception_groups=True) as nursery:
-            await nursery.start(raise_after_started)
-    assert len(exc.value.exceptions) == 1
-    assert isinstance(exc.value.exceptions[0], ValueError)
-
-
-async def test_no_strict_after_started() -> None:
-    with pytest.raises(ValueError):
-        async with trio.open_nursery(strict_exception_groups=False) as nursery:
-            await nursery.start(raise_after_started)
+@pytest.mark.parametrize("strict", [False, True])
+@pytest.mark.parametrize("raiser", [raise_before, raise_after_started])
+async def test_strict_before_started(
+    strict: bool, raiser: Callable[[], Awaitable[None]]
+) -> None:
+    with pytest.raises(BaseExceptionGroup if strict else ValueError) as exc:
+        await main(raiser, strict)
+    if strict:
+        _check_exception(exc)
 
 
 # it was only when run from `trio.run` that the double wrapping happened
-def test_trio_run_strict_before_started() -> None:
-    async def main() -> None:
-        async with trio.open_nursery() as nursery:
-            await nursery.start(raise_before)
-
-    with pytest.raises(BaseExceptionGroup) as exc:
-        trio.run(main, strict_exception_groups=True)
-    assert len(exc.value.exceptions) == 1
-    assert isinstance(exc.value.exceptions[0], ValueError)
-
-
-def test_trio_run_strict_after_started() -> None:
-    async def main() -> None:
-        async with trio.open_nursery() as nursery:
-            await nursery.start(raise_after_started)
-
-    with pytest.raises(BaseExceptionGroup) as exc:
-        trio.run(main, strict_exception_groups=True)
-    assert len(exc.value.exceptions) == 1
-    assert isinstance(exc.value.exceptions[0], ValueError)
-
-
-def test_trio_run_no_strict_before_started() -> None:
-    async def main() -> None:
-        async with trio.open_nursery() as nursery:
-            await nursery.start(raise_before)
-
-    with pytest.raises(ValueError):
-        trio.run(main, strict_exception_groups=False)
-
-
-def test_trio_run_no_strict_after_started() -> None:
-    async def main() -> None:
-        async with trio.open_nursery() as nursery:
-            await nursery.start(raise_after_started)
-
-    with pytest.raises(ValueError):
-        trio.run(main, strict_exception_groups=False)
+@pytest.mark.parametrize("strict", [False, True])
+@pytest.mark.parametrize("raiser", [raise_before, raise_after_started])
+def test_trio_run_strict_before_started(
+    strict: bool, raiser: Callable[[], Awaitable[None]]
+) -> None:
+    with pytest.raises(BaseExceptionGroup if strict else ValueError) as exc:
+        trio.run(main, raiser, strict_exception_groups=strict)
+    if strict:
+        _check_exception(exc)
 
 
 # TODO: this shouldn't doublewrap, but should it modify the exception/traceback in any way?
 def test_trio_run_strict_exceptiongroup_before_started() -> None:
-    async def main() -> None:
-        async with trio.open_nursery() as nursery:
-            await nursery.start(raise_custom_exception_group_before)
+    async def raise_custom_exception_group_before(
+        *, task_status: TaskStatus[None]
+    ) -> None:
+        raise ExceptionGroup("my group", [ValueError()])
 
     with pytest.raises(BaseExceptionGroup) as exc:
-        trio.run(main, strict_exception_groups=True)
-    assert len(exc.value.exceptions) == 1
-    assert isinstance(exc.value.exceptions[0], ValueError)
+        trio.run(
+            main, raise_custom_exception_group_before, strict_exception_groups=True
+        )
+    _check_exception(exc)
