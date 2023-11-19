093371833a5882bb27a5568f660e404b89cb4a90
Spencer Brown <spencerb21@live.com>
2023-11-19 11:38:16 +1000

    Bump Mypy version

--- src/trio/_core/_entry_queue.py
+++ src/trio/_core/_entry_queue.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import threading
 from collections import deque
-from typing import TYPE_CHECKING, Callable, Iterable, NoReturn, Tuple
+from typing import TYPE_CHECKING, Callable, NoReturn, Tuple
 
 import attr
 
