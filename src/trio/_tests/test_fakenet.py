3cbcf4a78f2075ff92d9ffd4525680c80e0e672c
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-11-30 20:54:50 -0600

    Fix unbalanced parenthesis

--- src/trio/_tests/test_fakenet.py
+++ src/trio/_tests/test_fakenet.py
@@ -234,11 +234,11 @@ async def test_not_implemented_functions() -> None:
     ):
         s1.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, False)
     with pytest.raises(
-        OSError, match=r"^FakeNet doesn't implement setsockopt\(\d+, \d+, \.\.\.)$"
+        OSError, match=r"^FakeNet doesn't implement setsockopt\(\d+, \d+, \.\.\.\)$"
     ):
         s1.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, True)
     with pytest.raises(
-        OSError, match=r"^FakeNet doesn't implement setsockopt\(\d+, \d+, \.\.\.)$"
+        OSError, match=r"^FakeNet doesn't implement setsockopt\(\d+, \d+, \.\.\.\)$"
     ):
         s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
