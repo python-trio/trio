0cb1b98d13d7a648e946338312dcc3247a0bbb7d
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-11-30 19:37:52 -0600

    Fix a few remaining windows match TODOs

--- src/trio/_tests/test_wait_for_object.py
+++ src/trio/_tests/test_wait_for_object.py
@@ -63,7 +63,7 @@ async def test_WaitForMultipleObjects_sync() -> None:
     handle1 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
     handle2 = kernel32.CreateEventA(ffi.NULL, True, False, ffi.NULL)
     kernel32.CloseHandle(handle2)
-    with pytest.raises(OSError, match="TODO: replace with exception"):
+    with pytest.raises(OSError, match=r"^\[WinError 6\] The handle is invalid$"):
         WaitForMultipleObjects_sync(handle1, handle2)
     kernel32.CloseHandle(handle1)
     print("test_WaitForMultipleObjects_sync close second OK")
