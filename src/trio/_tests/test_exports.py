e1edda0b15e11f50234bd62dc71ac6a65968bf94
Spencer Brown <spencerb21@live.com>
2023-12-01 15:08:23 +1000

    Mypy does see copy/deepcopy now, this is no longer required

--- src/trio/_tests/test_exports.py
+++ src/trio/_tests/test_exports.py
@@ -422,13 +422,10 @@ def test_static_tool_sees_class_members(
         if (
             tool == "mypy"
             and enum.Enum in class_.__mro__
-            and sys.version_info >= (3, 11)
+            and sys.version_info >= (3, 12)
         ):
-            extra.remove("__copy__")
-            extra.remove("__deepcopy__")
-            if sys.version_info >= (3, 12):
-                # Another attribute, in 3.12+ only.
-                extra.remove("__signature__")
+            # Another attribute, in 3.12+ only.
+            extra.remove("__signature__")
 
         # TODO: this *should* be visible via `dir`!!
         if tool == "mypy" and class_ == trio.Nursery:
