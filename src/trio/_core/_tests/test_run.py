17afaab07a91fd36f84d5f17570644e35d4b34c8
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-03-14 23:24:50 -0500

    Maintain partial results
    Co-authored-by: Spencer Brown <spencerb21@live.com>

--- src/trio/_core/_tests/test_run.py
+++ src/trio/_core/_tests/test_run.py
@@ -2044,7 +2044,9 @@ async def test_nursery_stop_async_iteration() -> None:
 
             return items
 
-    result: list[list[int]] = [vals async for vals in async_zip(it(4), it(2))]
+    result: list[list[int]] = []
+    async for vals in async_zip(it(4), it(2)):
+        result.append(vals)  # noqa: PERF402  # maintain partial results
     assert result == [[0, 0], [1, 1]]
 
 
