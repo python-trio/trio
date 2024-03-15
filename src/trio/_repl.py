50ccf770ad098a71dd62199490b28fd3a2101645
Clint Lawrence <clint.lawrence@gmail.com>
2024-03-15 13:04:19 +1100

    Fix index of dict type, in typing.cast

--- src/trio/_repl.py
+++ src/trio/_repl.py
@@ -8,6 +8,7 @@ import types
 import typing
 import warnings
 from code import InteractiveConsole
+from typing import Dict
 
 import trio
 import trio.lowlevel
@@ -24,7 +25,9 @@ class TrioInteractiveConsole(InteractiveConsole):
             # However FunctionType expects a dict. We know our copy of
             # locals will be a dict due to the annotation on repl_locals in __init__
             # so the cast is safe.
-            func = types.FunctionType(code, typing.cast(dict[str, object], self.locals))
+            # In addition, we need to use typing.Dict here, because this is _not_ an
+            # annotation, so from __future__ import annotations doesn't help.
+            func = types.FunctionType(code, typing.cast(Dict[str, object], self.locals))
             try:
                 coro = func()
             except BaseException as e:
