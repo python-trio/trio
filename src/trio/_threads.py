23b2c87ffe1fbe9052551969df546c9484aa56c8
Alla Barbalat <allabarbalat@gmail.com>
2024-05-18 13:57:32 -0400

    updated hazmat to lowlevel in function for getting token for from_thread_run to reflect updated documentation

--- src/trio/_threads.py
+++ src/trio/_threads.py
@@ -589,7 +589,7 @@ def from_thread_run(
           to enter Trio, or if you want to use a new system task to call ``afn``,
           maybe to avoid the cancellation context of a corresponding
           `trio.to_thread.run_sync` task. You can get this token from
-          :func:`trio.hazmat.current_trio_token`.
+          :func:`trio.lowlevel.current_trio_token`.
     """
     return _send_message_to_trio(trio_token, Run(afn, args))
 
