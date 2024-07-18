d3b388cf37ec066b38119ab415f8b3a284055b98
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-07-17 23:13:15 -0500

    Update uv and remove `--no-strip-markers` Apparently `--universal` implies `--no-strip-markers`

--- check.sh
+++ check.sh
@@ -78,10 +78,10 @@ fi
 
 # Check pip compile is consistent
 echo "::group::Pip Compile - Tests"
-uv pip compile --universal --no-strip-markers --python-version=3.8 test-requirements.in -o test-requirements.txt
+uv pip compile --universal --python-version=3.8 test-requirements.in -o test-requirements.txt
 echo "::endgroup::"
 echo "::group::Pip Compile - Docs"
-uv pip compile --universal --no-strip-markers --python-version=3.8 docs-requirements.in -o docs-requirements.txt
+uv pip compile --universal --python-version=3.8 docs-requirements.in -o docs-requirements.txt
 echo "::endgroup::"
 
 if git status --porcelain | grep -q "requirements.txt"; then
