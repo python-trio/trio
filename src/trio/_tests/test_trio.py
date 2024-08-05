d41cca18dba53e32117857785024cb05471f8cec
EXPLOSION <git@helvetica.moe>
2024-08-05 13:53:12 +0000

    Fix testcase

--- src/trio/_tests/test_trio.py
+++ src/trio/_tests/test_trio.py
@@ -1,5 +1,8 @@
-import sys
+def test_trio_import():
+    import sys
 
-for module in list(sys.modules.keys()):
-    if module.startswith("trio"):
-        del sys.modules[module]
+    for module in list(sys.modules.keys()):
+        if module.startswith("trio"):
+            del sys.modules[module]
+    
+    import trio
