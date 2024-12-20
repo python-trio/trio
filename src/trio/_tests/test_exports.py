becdf2a4c5dca6a634ee2114cfa367c39e8c59e4
Thomas Grainger <tagrain@gmail.com>
2024-12-20 11:18:58 +0000

    assert there's no node in cached_type_info

--- src/trio/_tests/test_exports.py
+++ src/trio/_tests/test_exports.py
@@ -384,8 +384,11 @@ def test_static_tool_sees_class_members(
         elif tool == "mypy":
             # load the cached type information
             cached_type_info = cache_json["names"][class_name]
-            if "node" not in cached_type_info:
-                cached_type_info = lookup_symbol(cached_type_info["cross_ref"])
+            # previously this was an 'if' but it seems it's no longer possible
+            # for this cache to contain 'node', if this assert raises for you
+            # please let us know!
+            assert "node" not in cached_type_info
+            cached_type_info = lookup_symbol(cached_type_info["cross_ref"])
 
             assert "node" in cached_type_info
             node = cached_type_info["node"]
