8f05dbffa47f75afc31d7e1762328ac49cd5babf
Thomas Grainger <tagrain@gmail.com>
2024-12-20 10:35:47 +0000

    actually enter the cmgr

--- src/trio/_core/_tests/test_io.py
+++ src/trio/_core/_tests/test_io.py
@@ -412,8 +412,11 @@ async def test_io_manager_kqueue_monitors_statistics() -> None:
             check(expected_monitors=0, expected_readers=1, expected_writers=0)
 
             with _core.monitor_kevent(a1.fileno(), select.KQ_FILTER_READ) as q:
-                with pytest.raises(_core.BusyResourceError):
-                    _core.monitor_kevent(a1.fileno(), select.KQ_FILTER_READ)
+                with (
+                    pytest.raises(_core.BusyResourceError),
+                    _core.monitor_kevent(a1.fileno(), select.KQ_FILTER_READ),
+                ):
+                    pass
                 check(expected_monitors=1, expected_readers=1, expected_writers=0)
                 b1.send(b"\x00")
                 assert len(await q.get_batch()) == 1
