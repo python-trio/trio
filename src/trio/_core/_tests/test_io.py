5344e71e5b64977f4fac3b30fdb46e4733ef8138
Thomas Grainger <tagrain@gmail.com>
2024-12-20 10:28:14 +0000

    test more of kqueue

--- src/trio/_core/_tests/test_io.py
+++ src/trio/_core/_tests/test_io.py
@@ -411,8 +411,12 @@ async def test_io_manager_kqueue_monitors_statistics() -> None:
             # 1 for call_soon_task
             check(expected_monitors=0, expected_readers=1, expected_writers=0)
 
-            with trio.lowlevel.monitor_kevent(a1.fileno(), select.KQ_FILTER_READ):
+            with _core.monitor_kevent(a1.fileno(), select.KQ_FILTER_READ) as q:
+                with pytest.raises(_core.BusyResourceError):
+                    _core.monitor_kevent(a1.fileno(), select.KQ_FILTER_READ)
                 check(expected_monitors=1, expected_readers=1, expected_writers=0)
+                b1.send(b"\x00")
+                assert len(await q.get_batch()) == 1
 
             check(expected_monitors=0, expected_readers=1, expected_writers=0)
 
