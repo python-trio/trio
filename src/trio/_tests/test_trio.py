cb3de6a886c7a882da23b4b6ae2e96abe61436b5
A5rocks <git@helvetica.moe>
2025-01-21 14:16:57 +0900

    Actually deprecate `trio.__version__`

--- src/trio/_tests/test_trio.py
+++ src/trio/_tests/test_trio.py
@@ -6,3 +6,12 @@ def test_trio_import() -> None:
             del sys.modules[module]
 
     import trio  # noqa: F401
+
+
+def test_trio_version_deprecated() -> None:
+    import pytest
+
+    import trio
+
+    with pytest.warns(DeprecationWarning, match="^trio.__version__ is deprecated"):
+        _ = trio.__version__
