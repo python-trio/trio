a31d5364a83ccdaf955f7921446faa083644cd99
A5rocks <git@helvetica.moe>
2025-06-11 18:38:07 +0000

    Final change

--- ci.sh
+++ ci.sh
@@ -139,7 +139,7 @@ echo "::group::Coverage"
 coverage combine --rcfile ../pyproject.toml
 cd ..  # coverage needs to be in the folder containing src/trio
 cp empty/.coverage .
-coverage report -m --rcfile ./pyproject.toml --debug=pathmap
+coverage report -m --rcfile ./pyproject.toml
 coverage xml --rcfile ./pyproject.toml
 
 # Remove the LSP again; again we want to do this ASAP to avoid
