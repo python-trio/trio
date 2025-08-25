1ea6615510616b4f0b447b93dedd670c526f7e94
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2025-08-24 22:10:19 -0500

    Switch to deprecated warning

--- src/trio/_sync.py
+++ src/trio/_sync.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import math
-from typing import TYPE_CHECKING, NoReturn, Protocol
+from typing import TYPE_CHECKING, Protocol
 
 import attrs
 
@@ -16,6 +16,7 @@ from ._core import (
     enable_ki_protection,
     remove_parking_lot_breaker,
 )
+from ._depreciate import warn_deprecated
 from ._util import final
 
 if TYPE_CHECKING:
@@ -112,11 +113,16 @@ class Event:
         """
         return EventStatistics(tasks_waiting=len(self._tasks))
 
-    def __bool__(self) -> NoReturn:
-        """Raise NotImplementedError."""
-        raise NotImplementedError(
-            "Trio events cannot be treated as bools; consider using 'event.is_set()'"
+    def __bool__(self) -> True:
+        """Return True and raise warning."""
+        warn_deprecated(
+            self.__bool__,
+            "0.30.1",
+            issue=3238,
+            instead=self.is_set,
+            use_triodeprecationwarning=True,
         )
+        return True
 
 
 class _HasAcquireRelease(Protocol):
