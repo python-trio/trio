67b5715c6553ca366a5978de5714bceb5f35b8c6
pctablet505 <pctablet505@gmail.com>
2026-07-27 15:12:51 +0530

    Rename max_buffer_used to peak_buffer_used per review feedback
    max_buffer_used read as a configured limit rather than a runtime metric,
    easily confused with max_buffer_size. peak_buffer_used pairs more clearly
    with the existing current_buffer_used field.

--- src/trio/_channel.py
+++ src/trio/_channel.py
@@ -96,7 +96,7 @@ class open_memory_channel(tuple["MemorySendChannel[T]", "MemoryReceiveChannel[T]
       channel (summing over all clones).
     * ``tasks_waiting_receive``: The number of tasks blocked in ``receive`` on
       this channel (summing over all clones).
-    * ``max_buffer_used``: The largest number of items that have been in the
+    * ``peak_buffer_used``: The largest number of items that have been in the
       buffer at once since the channel was created.
     """
 
@@ -144,7 +144,7 @@ class MemoryChannelStatistics:
     tasks_waiting_receive: int
     """The number of tasks currently blocked waiting to receive."""
 
-    max_buffer_used: int
+    peak_buffer_used: int
     """The largest number of items that have been in the buffer at once
     since the channel was created."""
 
@@ -161,7 +161,7 @@ class MemoryChannelState(Generic[T]):
     # {task: None}
     receive_tasks: OrderedDict[Task, None] = attrs.Factory(OrderedDict)
     # The largest len(self.data) has ever been
-    max_buffer_used: int = 0
+    peak_buffer_used: int = 0
 
     def statistics(self) -> MemoryChannelStatistics:
         return MemoryChannelStatistics(
@@ -171,7 +171,7 @@ class MemoryChannelState(Generic[T]):
             open_receive_channels=self.open_receive_channels,
             tasks_waiting_send=len(self.send_tasks),
             tasks_waiting_receive=len(self.receive_tasks),
-            max_buffer_used=self.max_buffer_used,
+            peak_buffer_used=self.peak_buffer_used,
         )
 
 
@@ -220,8 +220,8 @@ class MemorySendChannel(SendChannel[SendType], metaclass=NoPublicConstructor):
             trio.lowlevel.reschedule(task, Value(value))
         elif len(self._state.data) < self._state.max_buffer_size:
             self._state.data.append(value)
-            self._state.max_buffer_used = max(
-                self._state.max_buffer_used,
+            self._state.peak_buffer_used = max(
+                self._state.peak_buffer_used,
                 len(self._state.data),
             )
         else:
